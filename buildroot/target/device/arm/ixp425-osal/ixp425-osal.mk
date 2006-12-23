30f45265f5ec5447facbd5b2f04a4eb3a8937aa3
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2006-12-23 02:03:38 +0000

    Ethernet driver now compiles and installs into target system.
    It's currently untested.  Testing should be easier than the build!

--- buildroot/target/device/arm/ixp425-osal/ixp425-osal.mk
+++ buildroot/target/device/arm/ixp425-osal/ixp425-osal.mk
@@ -11,7 +11,7 @@ IXP425_OSAL_KMOD_LOC := $(TARGET_DIR)/lib/modules/$(DOWNLOAD_LINUX_VERSION)/extr
 IXP425_OSAL_COMPONENTS := "qmgr npeMh npeDl ethAcc ethDB ethMii featureCtrl osServices oslinux"
 
 IXP425_OSAL_HEADER_DIR := $(STAGING_DIR)/include/linux/ixp4xx-csr
-
+IXP425_OSAL_MODULE_SYMVER := $(IXP425_OSAL_DIR)/ixp400_xscale_sw/lib/linuxbe/Module.symvers
 
 $(IXP425_OSAL_DIR)/.source:
 	#unzip the files
@@ -29,15 +29,21 @@ $(IXP425_OSAL_KMOD): $(IXP425_OSAL_DIR)/.patched
 	$(MAKE) -C $(IXP425_OSAL_DIR)/ixp_osal IX_TARGET=linuxbe CC=$(TARGET_CC) LD=$(KERNEL_CROSS)ld AR=$(KERNEL_CROSS)ar IX_DEVICE=ixp425 LINUX_SRC=$(LINUX_DIR) LINUX_CROSS_COMPILE=$(KERNEL_CROSS) libosal module
 	$(MAKE) -C $(IXP425_OSAL_DIR)/ixp400_xscale_sw AR=$(KERNEL_CROSS)ar IX_TARGET=linuxbe IX_XSCALE_SW=$(IXP425_OSAL_DIR)/ixp400_xscale_sw linuxbe_COMPONENTS=$(IXP425_OSAL_COMPONENTS) linuxbe_CODELETS_COMPONENTS="" IX_DEVICE=ixp425 IX_INCLUDE_MICROCODE=1 IX_UTOPIAMODE=0 IX_MPHYSINGLEPORT=1 LINUX_SRC=$(LINUX_DIR) LINUX_CROSS_COMPILE=$(KERNEL_CROSS) OSAL_DIR=$(IXP425_OSAL_DIR)/ixp_osal OSAL_IMAGE=$(IXP425_OSAL_DIR)/ixp_osal/lib/ixp425/linux/linuxbe/libosal.a OSAL_MODULE=$(IXP425_OSAL_DIR)/ixp_osal/lib/ixp425/linux/linuxbe/ixp_osal.o lib/linuxbe/ixp400.o
 
+$(IXP425_OSAL_MODULE_SYMVER): $(IXP425_OSAL_KMOD)
+	#We have to cd here. modpost seems to break otherwise.
+	#FIXME: The kernel docs indicate that the kbuild system should generate the Module.symvers file automatically.
+	#rob: I think this is because the feature was introducted in kernels later than 2.6.15.
+	(cd $(IXP425_OSAL_DIR)/ixp400_xscale_sw/lib/linuxbe;  $(LINUX_DIR)/scripts/mod/modpost -o Module.symvers ixp400.ko)
+
 $(IXP425_OSAL_KMOD_LOC): $(IXP425_OSAL_KMOD)
 	$(MAKE) -C $(LINUX_DIR) CROSS_COMPILE=$(KERNEL_CROSS) ARCH=$(LINUX_KARCH) INSTALL_MOD_PATH=$(TARGET_DIR) M=$(IXP425_OSAL_DIR)/ixp400_xscale_sw/lib/linuxbe modules_install
 	$(DEPMOD) -b $(TARGET_DIR)/lib/modules/$(DOWNLOAD_LINUX_VERSION) -F $(LINUX_DIR)/System.map
 
-$(IXP425_OSAL_HEADER_DIR)/.installed: $(IXP425_OSAL_KMOD)
+$(IXP425_OSAL_HEADER_DIR)/.installed: 
 	mkdir -p $(IXP425_OSAL_HEADER_DIR)
 	cp $(IXP425_OSAL_DIR)/ixp400_xscale_sw/src/include/*.h $(IXP425_OSAL_HEADER_DIR)
 	touch $(IXP425_OSAL_HEADER_DIR)/.installed
 
-ixp425_osal: linux $(IXP425_OSAL_KMOD_LOC) $(IXP425_OSAL_HEADER_DIR)/.installed
+ixp425_osal: linux $(IXP425_OSAL_KMOD_LOC) $(IXP425_OSAL_MODULE_SYMVER)  $(IXP425_OSAL_HEADER_DIR)/.installed
 
 
