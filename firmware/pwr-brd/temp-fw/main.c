93715ea4714ff1bacb18b4e09fcd73f797a64e3a
trb105 <trb105@98f67ba8-b320-0410-bda0-b753e294843c>
2007-03-12 01:16:55 +0000

    Updated Firmware - now boots slug on power-up

--- firmware/pwr-brd/temp-fw/main.c
+++ firmware/pwr-brd/temp-fw/main.c
@@ -1,25 +1,63 @@
 #include <p18f4550.h>
 
 long int i;
+void init(void);
+void delay(int time);
 
-void main (void)
-{
+
+//rc0 slug boot switch blip low to boot
+
 //rd0-4 switch
 //rd5-7 leds
 //re0 - big power motor fet thing
 //re1 - fet control servo rail
 //re2 - fet slug rail
 
-	TRISD= 0x0f;
-	PORTD = 0;//0b10100000;
-	TRISE = 0;
-	PORTE = 0b111; // turn all power rails on
+	
+
+
+void main (void)
+{
+	init();
+	
+	delay(20);
+	PORTCbits.RC0=0;
+	PORTD=0b00100000;
+	delay(20);
 	while ( 1 )
 	{
 		PORTD^= 0xFF;
 		
-	for (i=0;i<99999;i++);
+		delay(20);
 	
 	}//end while
 			
 }// end main
+
+
+void init(void)
+{
+	PORTC=0X01; // MUST BE BEFORE UNTRISTATING ELSE SLUG BOOT!!!
+	TRISA=0XFF;
+	TRISB=0XFF;
+	TRISC=0XFE;
+	TRISD= 0x0f;
+	TRISE = 0;
+
+	PORTD = 0;//0b10100000;
+	PORTE = 0b111; // turn all power rails on
+
+}
+
+void delay(int time)
+{
+	int sponge =0;
+	//good time about 20
+	i=0;
+	
+	for (i=0;i<(time*100);i++)
+	{
+		for(sponge=0;sponge<250;sponge++);
+	}
+
+}
