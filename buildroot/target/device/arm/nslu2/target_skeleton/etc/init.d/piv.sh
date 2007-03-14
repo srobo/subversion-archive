4c0d1475e6a1c9ebbe8754100f66c4b3b1d7a06f
trb105 <trb105@98f67ba8-b320-0410-bda0-b753e294843c>
2007-03-14 13:12:06 +0000

    added pivot root script

---
+++ buildroot/target/device/arm/nslu2/target_skeleton/etc/init.d/piv.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+while ! { dd if=/dev/sda of=/dev/null bs=1 count=1 ; }
+do
+sleep 1
+done
+
+
+mkdir /tmp/newroot
+mount /dev/sda /tmp/newroot/
+pivot_root /tmp/newroot/ /tmp/newroot/initrd/
+
+echo "pivoted"
+
+mount -a
+
+umount /initrd/proc 
+umount /initrd/dev/pts 
+umount /initrd/tmp
+
+killall syslogd klogd
+
+
+exec /etc/ecssr.sh
+
+
