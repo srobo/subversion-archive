87247fdc46f4df7e5c543d32576815dd7c824518
se204 <se204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-05-15 23:22:59 +0000

    Added an example of state machine coding

---
+++ kidcode/linear/vision.py
@@ -0,0 +1,10 @@
+class blobfinder:
+    def __init__(self):
+        print "Created a blob finder class"
+
+    def get_blobs(self):
+        """Get a list of blobs as (X, Y, Colour)"""
+
+        #Currently return 2 blobs
+        return [(20, 20, 0),
+                (100, 50, 1)]
