3b20d336009c3821b71b7941ea5654cd0b983fe8
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-07-18 21:30:44 +0000

    Add some function declarations to xbee-conn.h

--- xbee/libxbee/trunk/xbee-conn.h
+++ xbee/libxbee/trunk/xbee-conn.h
@@ -4,7 +4,28 @@
 #include <glib-object.h>
 #include <stdint.h>
 
+typedef uint8_t chan_num_t;
 
 
+struct xbee_conn_t;
+
+typedef struct xbee_conn_t XbeeConn;
+
+/* Open a connection to the server */
+XbeeConn* xbee_conn_new( char* addr );
+
+/* Close the connection */
+void xbee_conn_close( XbeeConn* xbc );
+
+/* Get a channel
+ * Returns 0 on error (?) */
+chan_num_t xbee_conn_get_channel( XbeeConn* xbc, 
+				  gboolean (*receive) ( XbeeConn*, uint8_t* data, uint16_t len ) );
+
+/* Release a channel */
+void xbee_conn_release_channel( XbeeConn* xbc, channel_num_t channel );
+
+/* Transmit some data */
+gboolean xbee_conn_transmit( XbeeConn* xbc, uint8_t* data, uint16_t len );
 
 #endif
