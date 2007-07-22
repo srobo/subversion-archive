a072b9922177c5f5b38090dcb66d84a327906222
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-07-22 21:34:14 +0000

    Make xbee_module_read_frame static

--- xbee/trunk/xbd/xbee-module.c
+++ xbee/trunk/xbd/xbee-module.c
@@ -24,7 +24,7 @@ gboolean xbee_module_proc_incoming( XbeeModule* xb );
  * When a full frame is achieved, it returns 0.
  * When a full frame has not been acheived, it returns 1.
  * When an error occurs, it returns -1 */
-int xbee_module_read_frame( XbeeModule* xb  );
+static int xbee_module_read_frame( XbeeModule* xb  );
 
 static uint8_t xbee_module_checksum( uint8_t* buf, uint16_t len );
 
