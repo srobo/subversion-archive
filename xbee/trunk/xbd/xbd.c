16e6f60d4a98e161e267eb6981c594ff0523b96e
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-07-29 19:11:06 +0000

    Config file and command line arguments support

--- xbee/trunk/xbd/xbd.c
+++ xbee/trunk/xbd/xbd.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <glib.h>
+#include <stdlib.h>
 #include "xbee-server.h"
+
+void parse_config_file( void );
+void config_create( int argc, char **argv );
+
+static gchar *config = NULL, 
+	*sdev = NULL,
+	*listen = NULL;
+
+static GOptionEntry entries[] = 
+{
+	{ "config", 'c', 0, G_OPTION_ARG_FILENAME, &config, "Config file filename" },
+	{ "serial", 's', 0, G_OPTION_ARG_FILENAME, &sdev, "Serial device" },
+	{ NULL }
+};
 
 int main( int argc, char** argv )
 {
@@ -11,16 +26,12 @@ int main( int argc, char** argv )
 
 	g_type_init();
 
-	if( argc < 2 )
-	{
-		fprintf( stderr, "Not enough arguments\n" );
-		return 1;
-	}
+	config_create( argc, argv );
 
 	ml = g_main_loop_new( NULL, FALSE );
 	context = g_main_loop_get_context( ml );
 
-	xb = xbee_module_open( argv[1], context );
+	xb = xbee_module_open( sdev, context );
 	g_return_val_if_fail( xb != NULL, 1 );
 
 	server = xbee_server_new( context );
@@ -35,3 +46,56 @@ int main( int argc, char** argv )
 
 	return 0;
 }
+
+void config_create( int argc, char **argv )
+{
+	GOptionContext *opt_context;
+	GError *error = NULL;
+	
+	opt_context = g_option_context_new( "" );  
+	g_option_context_set_summary( opt_context, "xbee server" );
+	g_option_context_add_main_entries(opt_context, entries, NULL);
+
+	if(!g_option_context_parse( opt_context, &argc, &argv, &error ))
+	{
+		g_print("Error: %s\n", error->message);
+		exit(1);
+	}
+
+	if( config != NULL )
+		parse_config_file();
+	
+	if( sdev == NULL )
+	{
+		g_printerr( "Error: No serial port specified\n" );
+		exit(3);
+	}
+
+	if( listen == NULL )
+		listen = "/tmp/xbee";
+}
+
+void parse_config_file( void )
+{
+	GKeyFile *f;
+	GError *error = NULL;
+
+	f = g_key_file_new();
+	if( !g_key_file_load_from_file( f, 
+					config,
+					G_KEY_FILE_NONE,
+					&error ) )
+		g_print("Failed to parse config file: %s\n", error->message);
+
+	
+	if( g_key_file_has_group( f, "server" ) 
+	    && g_key_file_has_key( f, "server",
+				   "listen", NULL ) )
+		listen = g_key_file_get_value(f, "server", "listen", NULL);
+
+	if( g_key_file_has_group( f, "xbee" ) 
+	    && g_key_file_has_key( f, "xbee",
+				   "sdev", NULL ) )
+		sdev = g_key_file_get_value(f, "xbee", "sdev", NULL);
+}
+
