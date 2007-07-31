695064eaa671fd5fd429ddea4da9547bd6d17444
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-07-31 01:41:33 +0000

    Remove superfluous comment in makefile

--- xbee/trunk/libxbee/include.mk
+++ xbee/trunk/libxbee/include.mk
@@ -3,7 +3,6 @@ CLEAN += $(addprefix libxbee/,libxbee.a xbee-conn.o test)
 libxbee/test: libxbee/test.c libxbee/libxbee.a
 
 libxbee/libxbee.a: libxbee/xbee-conn.o common/xb-fd-source.o
-#libxbee/libxbee.a: libxbee/xbee-conn.o
 	$(AR) r $@ $^
 
 
