b93bc11f28f1870a48f84157076744935c256eb2
pjs104 <pjs104@98f67ba8-b320-0410-bda0-b753e294843c>
2007-08-26 16:03:32 +0000

    Added channel handling functions and command line arguments

--- xbee/trunk/libxbee/test.c
+++ xbee/trunk/libxbee/test.c
@@ -1,13 +1,36 @@
 #include "xbee-conn.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <glib.h>
 
 #include "xbee-conn.h"
 
 gboolean tx( XbeeConn *xbc );
 void xbtest ( XbeeConn *xbc );
 void rx_data (uint8_t * data, uint16_t len, xbee_conn_info_t *info);
+void config_options (int argc, char **argv);
+
+static gchar *socket = NULL,
+	*address = NULL,
+//	*channel = NULL,
+	*receive = NULL,
+	*transmit = NULL;
+static int8_t channel = -1;
+static int16_t address = -1;
+
+
+static GOptionEntry entries[] = 
+{
+	{ "socket", 's', 0, G_OPTION_ARG_FILENAME, &socket, "Socket path to connect to" },
+	{ "address", 'a', 0, G_OPTION_ARG_INT, &address, "Last Byte of destination Address" },
+	{ "channel", 'c', 0, G_OPTION_ARG_INT, &channel, "Destination Channel" },
+	{ "receive", 'r', 0, G_OPTION_ARG_NONE, &receive, "Receive Data Only" },
+	{ "transmit", 't', 0, G_OPTION_ARG_NONE, &transmit, "Transmit Data Only" },
+	{ NULL }
+};
+
 
 int main( int argc, char** argv )
 {
@@ -17,50 +40,42 @@ int main( int argc, char** argv )
 	
 	xb_conn_callbacks_t callbacks;
 
+	config_options ( argc, argv );
+
 	ml = g_main_loop_new( NULL, FALSE );
 	context = g_main_loop_get_context( ml );
 
 	g_type_init ();
 
-	if (argc < 2)
-	{
-		fprintf (stdout, "USAGE:\n");
-		fprintf (stdout, "  test [OPTIONS]\n\n");
-		fprintf (stdout, "Options:\n");
-		fprintf (stdout, " -s <socket>     Transmit/Receive via specified socket\n");
-		return (0);
-	}
-			
-	if (argc > 2)
-	{		
-		if (!strcmp (argv[1], "-s"))
-		{
-			xbc = xbee_conn_new (argv[2], context);
-			printf ("\nReceive mode activated:\n");
-			callbacks.rx_frame = &rx_data;
-			xbee_conn_register_callbacks (xbc, &callbacks);
-			printf ("\nBegin Transmit:\n");
-			g_timeout_add(3000, (GSourceFunc)tx, (gpointer)xbc);
-		}
-	}
-	
-	
+			xbc = xbee_conn_new (socket, context);
+			if (receive != NULL)
+			{
+				printf ("\nReceive mode activated:\n");
+				callbacks.rx_frame = &rx_data;
+				xbee_conn_register_callbacks (xbc, &callbacks);
+			}
+			else if (transmit != NULL)
+			{
+				printf ("\nBegin Transmit:\n");
+				g_timeout_add(3000, (GSourceFunc)tx, (gpointer)xbc);
+			}
+
 	g_main_loop_run( ml );
 
 	return 0;
 } 
 
-gboolean tx( XbeeConn *xbc )
+gboolean tx( XbeeConn *xbc)
 {
 	uint8_t data[] = {1,1,1,1,1,1,1,1,1,1,1};
 	xb_addr_t addr =
 		{
 			.type = XB_ADDR_64,
-			.addr = {0x00, 0x13, 0xA2, 0x00, 0x40, 0x09, 0x00, 0xA8}
+			.addr = {0x00, 0x13, 0xA2, 0x00, 0x40, 0x09, 0x00, address}
 		};
 	assert( xbc != NULL );
 
-	xbee_conn_transmit( xbc, addr, data, 11 );
+	xbee_conn_transmit( xbc, addr, data, 11, channel );
 	
 	return TRUE;
 }
@@ -119,3 +134,33 @@ void rx_data (uint8_t * data, uint16_t len, xbee_conn_info_t *info)
 	printf (" \n ");
 }
 
+void config_options ( int argc, char **argv )
+{
+	GOptionContext *opt_context;
+	GError *error = NULL;
+
+	opt_context = g_option_context_new ("");
+	g_option_context_set_summary ( opt_context, "Test file" );
+	g_option_context_add_main_entries(opt_context, entries, NULL);
+
+	if (!g_option_context_parse (opt_context, &argc, &argv, &error ))
+	{
+		g_print ("Error: %s\n", error->message);
+		exit (1);
+	}
+
+	if (transmit != NULL && (address <= 0 || channel <= 0))
+	{
+		g_print ("Value: %d %d\n", address, channel);
+		g_print ("No destination address or channel\n");
+		exit (1);
+	}
+
+	if (transmit == NULL && receive == NULL)
+	{
+		g_print ("No mode specified\n");
+		exit (1);
+	}
+
+
+}
