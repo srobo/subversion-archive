0f463abc4d705f9e8acc8e704e16dc06b387cc08
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-09-05 01:20:49 +0000

    Some constants that should be later migrated into msp430 libc

--- firmware/motor/trunk/common.h
+++ firmware/motor/trunk/common.h
@@ -1,2 +1,12 @@
 #include <msp430x22x4.h>
 
+/* These should be migrated to msp430 libc */
+#define UCSSEL_UCLKI UCSSEL_0
+#define UCSSEL_ACLK UCSSEL_1
+#define UCSSEL_SMCLK UCSSEL_2
+
+/* As should these */
+#define UCMODE_SPI_3PIN UCMODE_0
+#define UCMODE_SPI_4PIN_STE1 UCMODE_1
+#define UCMODE_SPI_4PIN_STE0 UCMODE_2
+#define UCMODE_I2C UCMODE_3
