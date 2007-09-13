1ca25ff25c5b50414a13abfff370b1df278cf407
trb105 <trb105@98f67ba8-b320-0410-bda0-b753e294843c>
2007-09-13 01:34:39 +0000

    added newer test files

---
+++ slug/utils/power/pwr-test-read.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "i2c-dev.h"
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//#define ADDRESS 0x55
+#define ADDRESS 0x3f
+
+typedef enum
+{
+	FALSE = 0, TRUE
+} bool;
+
+bool err_enable = TRUE;
+
+uint16_t readword(int fd, uint8_t cmd) {
+    signed int tmp;
+    tmp = i2c_smbus_read_word_data( fd, cmd );
+    if(tmp == -1){
+        printf("Error reading byte.\n");
+        printf("Got 0x%02x.\n");
+        return 0;
+    }
+
+    return tmp;
+}
+
+
+
+uint8_t readbyte(int fd, uint8_t cmd) {
+    signed int tmp;
+    tmp = i2c_smbus_read_byte_data( fd, cmd );
+    if(tmp == -1){
+        printf("Error reading byte.\n");
+        printf("Got 0x%02x.\n");
+        return 0;
+    }
+
+    return 0xFF & tmp;
+}
+
+int main( int argc, char** argv )
+{
+	int fd;
+
+	fd = open( "/dev/i2c-0", O_RDWR );
+
+	if( fd == -1 )
+	{
+		fprintf( stderr, "Failed to open /dev/`i2c-0: %m\n" );
+		return 1;
+	}
+
+
+
+	if( ioctl( fd, I2C_SLAVE, ADDRESS ) < 0 )
+	{
+		fprintf( stderr, "Failed to set slave address: %m\n" );
+		return 2;
+	}
+
+
+
+    if( ioctl( fd, I2C_PEC, 1) < 0)
+    {
+        fprintf( stderr, "Failed to enable PEC\n");
+        return 3;
+    }
+
+
+
+	while(1) 
+	  {
+        printf("Read dips as %x\n", readbyte(fd, 5));
+	printf("Read ID as %x\n", readbyte(fd, 0));
+	printf("Read rails as %x\n", readbyte(fd, 7));
+	printf("Read v as %x\n", readword(fd, 3));	
+	printf("Read I as %x\n", readword(fd, 4));
+        sleep(1);
+	  }
+	return 0;
+}
