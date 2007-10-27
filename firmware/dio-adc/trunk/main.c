07896c83e279fb7b693af5eecd9435760e8a2268
cc1206 <cc1206@98f67ba8-b320-0410-bda0-b753e294843c>
2007-10-27 16:47:23 +0000

    Added initialisation function for ADC10

--- firmware/dio-adc/trunk/main.c
+++ firmware/dio-adc/trunk/main.c
@@ -16,8 +16,10 @@
 #include "common.h"
 #include "i2c.h"
 #include "init.h"
+#include <msp430/adc10.h>
 
 static int i = 0;
+void adc_init(void);
 
 void UNEXPECTED()
 {
@@ -26,11 +28,30 @@ void UNEXPECTED()
 int main( void )
 {
 	i = 0;
-
+	unsigned int adresult[8] ;
+	
 	/* Disable the watchdog timer */
 	WDTCTL = WDTHOLD | WDTPW;
 
 	init();
+	adc_init();
+	adresult[0] = 0;
+	
+	while(1){
+		if(! (ADC10CTL1 & ADC10BUSY)){
+			adresult[0] = ADC10MEM;
+			ADC10CTL0  |= ADC10SC;	
+			
+		}
+	};
+}
 
-	while(1);
+void adc_init(void){
+			//Use Vcc and Vss as referance voltages, 
+	ADC10CTL0 = SREF_AVCC_AVSS| ADC10SHT_DIV64 | ADC10SR | ADC10ON | ENC | ADC10SC;	
+	ADC10CTL1 = ADC10DIV_7;
+	ADC10AE = 0xFF;		//Enable all inputs as Analogue Inputs
+	P2SEL |= 0x1F;		//Set A0 - A4 as Input Pins
+	
+	
 }
