ad4cb2984da5b1ad2bfbeccaf7e4662b36c669f2
rds204 <rds204@98f67ba8-b320-0410-bda0-b753e294843c>
2007-10-29 00:22:56 +0000

    Allow setting of user to bind as

--- web/userman/sr/sr_ldap.py
+++ web/userman/sr/sr_ldap.py
@@ -6,12 +6,22 @@ bound = False
 if conn == None:
     conn = ldap.initialize("ldap://127.0.0.1/")
 
+def default_pass():
+    return ("cn=Manager,o=sr",getpass.getpass())
+
+user_callback = default_pass
+
+def set_userinfo( fn ):
+    global user_callback
+    user_callback = fn
+
 def bind():
-    global bound, conn
+    global bound, conn, user_callback
 
     if not bound:
+        info = user_callback()
         try:
-            conn.simple_bind_s( "cn=Manager,o=sr", getpass.getpass() )
+            conn.simple_bind_s( info[0], info[1] )
         except ldap.INVALID_CREDENTIALS:
             print "Incorrect password"
             sys.exit(1)
