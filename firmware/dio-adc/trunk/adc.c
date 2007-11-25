a7fede8e9267ab34dd3cf6a51845dfe2797c9433
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2007-11-25 23:35:49 +0000

    Correctly initialised all registers concerning Analogue to digital conversion. DTC is set up for one block transfer, conversions are automatically stored in general registers, allowing them to be accessed easily on interrupt

--- firmware/dio-adc/trunk/adc.c
+++ firmware/dio-adc/trunk/adc.c
@@ -20,11 +20,35 @@
 #include <signal.h>
 
 void adc_init(void){
-			//Use Vcc and Vss as referance voltages, 
-	ADC10CTL0 = SREF_AVCC_AVSS| ADC10SHT_DIV64 | ADC10SR | ADC10ON | ENC | ADC10SC | ADC10IE | MSC | ;	
-	ADC10CTL1 = ADC10DIV_7 | INCH_7 | CONSEQ_3;	//Set to single conversion mode
+
+	int block[8];		//8 bit buffer for DTC block
+
+			//Setup ADC10CTL0 register 
+	ADC10CTL0 = SREF_AVCC_AVSS ;	//use vcc/vss as references
+	ADC10CTL0 |= ADC10SHT_DIV64 ;	//sample and hold time of 64clk cycles
+	ADC10CTL0 |= ADC10SR;		//use lowest sample rate
+	ADC10CTL0 |= ADC10ON;		//enable ADC 
+	ADC10CTL0 |= ENC;		//enable conversion
+	ADC10CTL0 |= ADC10SC;		//start conversion
+	ADC10CTL0 |= ADC10IE;		//enable interrupts
+	ADC10CTL0 |= MSC;		//multiple sample conversion mode
+
+			//Setup ADC10CTL1 register
+	ADC10CTL1 = ADC10DIV_7;		// divide clock by 8 for conversion
+	ADC10CTL1 |= INCH_7; 		// convert channels A7 through A0
+	ADC10CTL1 |= CONSEQ_3;		// repeated sequence of conversion mode
+		
+			//Setup inputs
 	ADC10AE = 0xFF;		//Enable all inputs as Analogue Inputs
-	P2SEL |= 0x1F;		//Set A0 - A4 as Input Pins	
+	P2SEL |= 0x1F;			//Set A0 - A4 as Input Pins	
+	
+			//Setup Data Transfer Controller
+	ADC10DTC0 |= 0x00;		//Single block mode
+	ADC10DTC0 |= ADC10CT;		//Continuous data transfer
+	
+	ADC10DTC1 = 0x0F;		//8 transfers per block, (1 transfer per input)
+	
+	ADC10SA = &block;		//start address of block 
 }
 
 void adc_sample(void){
@@ -57,11 +81,6 @@ void adc_sample(void){
 
 interrupt (ADC10_VECTOR) intr_adc( void ){
 	
-	int adresult[8];
-	ADC10CTL0 |= ENC;
-	
-	adresult[0] = ADC10MEM
-	
-	//Data transfer control register must be set up, but not sure how?
+	//To be completed soon
 	
 }
