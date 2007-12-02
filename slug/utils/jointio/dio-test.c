359ee6e2fb8d7e5913fa94874ee7457dbb407c94
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2007-12-02 15:32:43 +0000

    Jointio test utility.

---
+++ slug/utils/jointio/dio-test.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "i2c-dev.h"
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ADDRESS 0x12
+#define POS0 3
+#define POS1 135
+
+typedef enum
+{
+	FALSE = 0, TRUE
+} bool;
+
+bool err_enable = TRUE;
+
+int main( int argc, char** argv )
+{
+	uint8_t d = POS0;
+	uint8_t val = 0;
+	int r;
+	uint8_t setting;
+	int fd;
+	uint8_t buf[16]; 
+
+	
+	
+
+	fd = open( "/dev/i2c-0", O_RDWR );
+
+/* 	if( argc != 2 ) */
+/* 	{ */
+/* 		printf("Usage: dio-test VALUE\n"); */
+/* 		return 1; */
+/* 	} */
+
+/* 	setting = atol( argv[1] ); */
+
+	if( fd == -1 )
+	{
+		fprintf( stderr, "Failed to open /dev/i2c-0: %m\n" );
+		return 1;
+	}
+
+	if( ioctl( fd, I2C_SLAVE, ADDRESS ) < 0 )
+	{
+		fprintf( stderr, "Failed to set slave address: %m\n" );
+		return 2;
+	}
+
+     if( ioctl( fd, I2C_PEC, 0) < 0) 
+     { 
+         fprintf( stderr, "Failed to enable PEC\n"); 
+         return 3; 
+    } 
+
+	r = i2c_smbus_write_byte(fd, 2);
+	if( r < 0 )
+		fprintf(stderr, "Failed to read dio pins\n");
+	else
+		printf( "Read %x from dio\n", r );
+	
+	printf("%d\n",read(fd, buf, 16));	
+	
+	int y;
+	uint16_t value;
+	for(y=0;y<8;y++)
+	{		
+		value = ((uint16_t)buf[2*y] << 8) |  (buf[(2*y)+1]);
+		printf("\n buf[%d] = %d", y, (int)value);
+		printf("\n %d %d", (int)buf[2*y], (int)buf[(2*y)+1]);
+	}
+	
+/* 	if( i2c_smbus_write_byte( fd, setting ) < 0 ) */
+/* 		fprintf( stderr, "Failed to set io\n" ); */
+	
+	return 0;
+}
