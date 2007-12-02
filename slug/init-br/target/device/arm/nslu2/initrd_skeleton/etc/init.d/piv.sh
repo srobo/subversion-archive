1d4f639037c3082bb5747b81a98ff0d5f7a9ecf0
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2007-12-02 15:17:37 +0000

    Change the pivoting script so that the slug takes 2 USB keys.

--- slug/init-br/target/device/arm/nslu2/initrd_skeleton/etc/init.d/piv.sh
+++ slug/init-br/target/device/arm/nslu2/initrd_skeleton/etc/init.d/piv.sh
@@ -6,9 +6,46 @@ do
 sleep 1
 done
 
+while ! { dd if=/dev/sdb of=/dev/null bs=1 count=1 ; }
+do
+sleep 1
+done
+
+# Work out which disk it's from
+mount /dev/sda /mnt/testa
+mount /dev/sdb /mnt/testb
+
+newroot="not found"
+usrdev=
+
+echo testa:
+ls -al /mnt/testa
+
+echo testb:
+ls -al /mnt/testb
+
+if [ -f /mnt/testa/sr-root ]
+then
+    # Root's on sda
+newroot=/dev/sda
+usrdev=/dev/sdb
+
+umount /mnt/testb
+
+elif [ -f /mnt/testb/sr-root ]
+then
+    # Root's on sdb
+newroot=/dev/sdb
+usrdev=/dev/sda
+
+umount /mnt/testa
+fi
+
+mount
+echo "newroot is $newroot"
 
 mkdir /tmp/newroot
-mount /dev/sda /tmp/newroot/
+mount $newroot /tmp/newroot/
 pivot_root /tmp/newroot/ /tmp/newroot/initrd/
 
 echo "pivoted"
@@ -21,6 +58,8 @@ umount /initrd/tmp
 
 killall syslogd klogd
 
+mkdir -p /mnt/usr
+mount $usrdev /mnt/usr
 
 exec /etc/ecssr.sh
 
