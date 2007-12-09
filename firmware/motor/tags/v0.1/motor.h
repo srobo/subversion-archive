0a0574609f75227016eb9d46f9342c584ba59863
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2007-12-09 18:43:34 +0000

    Tag for version distributed with motor controllers.

---
+++ firmware/motor/tags/v0.1/motor.h
@@ -0,0 +1,47 @@
+/*   Copyright (C) 2007 Robert Spanton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+/* This is the motor layer. 
+   It sits on top of the PWM layer - handles feedback etc. */
+#ifndef __MOTOR_H
+#define __MOTOR_H
+#include "pwm.h"
+
+/* At the moment it just sets the pwm ratio equal to speed */
+
+typedef pwm_ratio_t speed_t;
+
+/* States a motor can be in */
+typedef enum
+{
+	M_OFF = 0,
+	M_FORWARD,
+	M_BACKWARD,
+	M_BRAKE
+} motor_state_t;
+
+/* Set the speed/state of a motor */
+void motor_set( uint8_t channel, speed_t speed,  motor_state_t state );
+
+/* Get the speed of a motor */
+speed_t motor_get_speed( uint8_t channel );
+
+/* Get the state of a motor */
+motor_state_t motor_get_state( uint8_t channel );
+
+/* Initialise the motors */
+void motor_init( void );
+
+#endif	/* __PWM_H */
