2c9c5b68a1a5f295384a4f778384f9ce1ba0a83d
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2008-02-01 21:35:51 +0000

    Remove some superfluous scripts from the initrd.

--- slug/init-br/target/device/arm/nslu2/initrd_skeleton/etc/init.d/piv.sh
+++ slug/init-br/target/device/arm/nslu2/initrd_skeleton/etc/init.d/piv.sh
@@ -6,46 +6,8 @@ do
 sleep 1
 done
 
-while ! { dd if=/dev/sdb of=/dev/null bs=1 count=1 ; }
-do
-sleep 1
-done
-
-# Work out which disk it's from
-mount /dev/sda /mnt/testa
-mount /dev/sdb /mnt/testb
-
-newroot="not found"
-usrdev=
-
-echo testa:
-ls -al /mnt/testa
-
-echo testb:
-ls -al /mnt/testb
-
-if [ -f /mnt/testa/sr-root ]
-then
-    # Root's on sda
-newroot=/dev/sda
-usrdev=/dev/sdb
-
-umount /mnt/testb
-
-elif [ -f /mnt/testb/sr-root ]
-then
-    # Root's on sdb
-newroot=/dev/sdb
-usrdev=/dev/sda
-
-umount /mnt/testa
-fi
-
-mount
-echo "newroot is $newroot"
-
 mkdir /tmp/newroot
-mount $newroot /tmp/newroot/
+mount /dev/sda /tmp/newroot/
 pivot_root /tmp/newroot/ /tmp/newroot/initrd/
 
 echo "pivoted"
@@ -58,9 +20,7 @@ umount /initrd/tmp
 
 killall syslogd klogd
 
-mkdir -p /mnt/usr
-mount $usrdev /mnt/usr
-
+#Source the startup script on the newly mounted root
 exec /etc/ecssr.sh
 
 
