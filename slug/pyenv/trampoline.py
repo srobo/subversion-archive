3756d3f4cb7229cde3b6df4ff3a27e3fa90086c8
senglish <senglish@98f67ba8-b320-0410-bda0-b753e294843c>
2008-02-02 22:56:57 +0000

    Adding simulator trampoline

--- slug/pyenv/trampoline.py
+++ slug/pyenv/trampoline.py
@@ -1,31 +1,15 @@
+import types
 import robot
-from events import *
+from events import TimeoutEvent
 import time
-
-BIG_TIME = 9999999999999
+import logging
 
 class Trampoline:
-    q = [] #A list of polling tasks to execute when main stalled
+    def __init__(self):
+        self.bgpolls = []
 
-    def addtask(self, task):
-        """
-        This function adds a task to the queue. Tasks must be generators.
-        """
-        task.next() #Advance generator to first yield
-        self.q.append(task)
-        return task
-    
-    def removetask(self, task):
-        self.q.remove(task)
-
-    def gettimeouttime(self, timeout):
-        if timeout > 0:
-            timeouttime = time.time() + timeout
-        else:
-            timeouttime = BIG_TIME #Largest possible time.
-        
-        return timeouttime
-
+    def addtask(self, poll):
+        self.bgpolls.append(poll)
 
     def schedule(self):
         """
@@ -34,40 +18,100 @@ class Trampoline:
         out.
         """
 
-        main = robot.main(self)
-        robot.currentevent = None
-        robot.eventsource = None
-        timeout = main.next() #Advance to the first yield statement.
-        timeouttime = self.gettimeouttime(timeout)
-        
-        while 1:
-            if time.time() > timeouttime: #Check to see if the yield has timed out
-                robot.currentevent = TimeoutEvent()
-                robot.eventsource = None
-                try:
-                    timeout = main.next()
-                    timeouttime = self.gettimeouttime(timeout)
-                except StopIteration:
-                    return
+        stack = []
 
-            for task in self.q:
-                try:
-                    result = task.next()
-                except StopIteration:
-                    self.q.remove(task)
+        robot.event = None
+        
+        stack.append(robot.main(corner=0,colour=0,game=0))
+        
+        while True:
+            #Process any background tasks
+            logging.debug("Stack: %s", stack)
+           
+            logging.debug("Running the function on the top of the stack")
+            try:
+                #Try to run the function on the top of the stack
+                #Extend to an empty list to force it to be a list
+                args = None
+                try:                    
+                    args = stack[-1].next() #Advance to the first yield statement
+                except AttributeError:
+                    #A function returned putting a None on the stack
+                    raise StopIteration
+                    
+                if isinstance(args, types.TupleType):
+                    args = list(args)
+                else:
+                    args = [args]
+                logging.debug("Got control, args = %s", str(args))
+            except StopIteration:
+                #Remove the function on the top of the stack
+                logging.debug("Removing a function from the stack")
+                stack.pop()
+                if len(stack) == 0:
+                    #Run out of things to trampoline
+                    logging.debug("Running background polls")
+                    for poll in self.bgpolls:
+                        try:
+                            poll.next()
+                        except StopIteration:
+                            self.bgpolls.remove(poll)
+                    #Put the main function back in...
+                    stack.append(robot.main(corner=0,colour=0,game=0))
+                    #Go back to start of while loop
                     continue
 
-                if result != None:
-                    #Got an event for the mainloop
-                    robot.currentevent = result
-                    robot.eventsource = task
-                    try:
-                        timeout = main.next()
-                    except StopIteration:
-                        return
-                    timeouttime = self.gettimeouttime(timeout)
-                    break #Break out of the for loop
-            
+            if args == None:
+                #Function returned or yielded nothing. Go round to top of loop
+                continue
+
+            if args[0].__class__ == types.FunctionType:
+                #Push the function onto the stack
+                #Passing the rest of the yield as arguments
+                logging.debug("Adding a function to the stack with args %s" % \
+                        args[1:])
+                stack.append(args[0](*args[1:]))
+            else:
+                polls = []
+                timeout = None
+                for arg in args:
+                    if arg.__class__ == types.GeneratorType:
+                        logging.debug("Adding a generator to the polls list")
+                        polls.append(arg)
+                    elif isinstance(arg, int) or isinstance(arg, float):
+                        logging.debug("Adding a timeout")
+                        timeout = time.time() + arg
+                
+                #Check we have something to wait on
+                if len(polls) > 0 or timeout != None:
+                    #Loop waiting for something to happen
+                    result = None
+                    while True:
+                        logging.debug("Running background polls")
+                        for poll in self.bgpolls:
+                            try:
+                                poll.next()
+                            except StopIteration:
+                                self.bgpolls.remove(poll)
+ 
+                        if timeout != None and timeout < time.time():
+                            #Timed out
+                            logging.debug("Timed out")
+                            result = TimeoutEvent(timeout)
+                        else:
+                            for poll in polls:
+                                try:
+                                    result = poll.next()
+                                except StopIteration:
+                                    polls.remove(poll)
+
+                        #See if anything happened
+                        if result != None:
+                            logging.debug("Got a result %s", result)
+                            robot.event = result
+                            #Break out of this inner while loop
+                            break
+                    
 if __name__ == "__main__":
     t = Trampoline()
     t.schedule()
