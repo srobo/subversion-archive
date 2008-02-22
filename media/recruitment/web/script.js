e17d01642c78b5e667ed64287b2c15abdbd87ae6
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2008-02-22 18:26:54 +0000

    add javascript image preloader

---
+++ media/recruitment/web/script.js
@@ -0,0 +1,17 @@
+//image preloading
+
+pic1 = new Image(200, 200);
+pic2 = new Image(200, 200);
+pic3 = new Image(200, 200);
+pic4 = new Image(200, 200);
+pic5 = new Image(200, 200);
+pic6 = new Image(200, 200);
+pic7 = new Image(200, 200);
+
+pic1.src = "images/join.png";
+pic2.src = "images/downloads.png";
+pic3.src = "images/roles.png";
+pic4.src = "images/why.png";
+pic5.src = "images/competition.png";
+pic6.src = "images/contact.png";
+pic7.src = "images/faq.png";
