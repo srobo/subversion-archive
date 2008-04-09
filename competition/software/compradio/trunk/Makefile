6adf1db145da5308042b03dd08337e8b5f7ff0ed
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2008-04-09 15:19:39 +0000

    Beginnings of a GUI.

--- competition/software/compradio/trunk/Makefile
+++ competition/software/compradio/trunk/Makefile
@@ -1,14 +1,18 @@
 CFLAGS := -Wall -O3
 LDFLAGS :=
 
-CFLAGS += `pkg-config --cflags libxb`
-LDFLAGS += `pkg-config --libs libxb`
+# CFLAGS += `pkg-config --cflags libxb`
+# LDFLAGS += `pkg-config --libs libxb`
 
-compradio: compradio.c teams.o
+CFLAGS := `pkg-config --cflags libglade-2.0`
+LDFLAGS := `pkg-config --libs libglade-2.0`
 
-teams.o: teams.c teams.h
+LDFLAGS += -Wl,--export-dynamic
+
+compradio: compradio.c compradio.glade
+	$(CC) compradio.c -o compradio $(CFLAGS) $(LDFLAGS)
 
 .PHONY: clean
 
 clean:
-	-rm -f compradio teams.o
+	-rm -f compradio
--- competition/software/compradio/trunk/compradio.c
+++ competition/software/compradio/trunk/compradio.c
@@ -1,13 +1,55 @@
 #include <stdio.h>
-#include <xbee-conn.h>
-#include "teams.h"
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+/* #include <xbee-conn.h> */
+
+/* Window destruction */
+gboolean main_window_destroy( GtkObject *object,
+			      gpointer user_data );
+
+/* Window deletion */
+gboolean main_window_delete_event( GtkWidget *widget,
+				   GdkEvent *event,
+				   gpointer user_data );
+
+
+/* The ping enable/disable checkbox */
+void on_check_ping_toggled( GtkToggleButton *togglebutton,
+			    gpointer user_data );
 
 int main( int argc, char** argv )
 {
-	g_type_init();
+	GladeXML *xml;
+	gtk_init( &argc, &argv );
 
-	teams_read( "./teams.xml" );
+	xml = glade_xml_new("compradio.glade", NULL, NULL);
 
+	glade_xml_signal_autoconnect(xml);
+
+	gtk_main();
 
 	return 0;
+}
+
+gboolean main_window_destroy( GtkObject *object,
+			      gpointer user_data )
+{
+	gtk_main_quit();
+}
+
+gboolean main_window_delete_event( GtkWidget *widget,
+				   GdkEvent *event,
+				   gpointer user_data )
+{
+	return FALSE;
+}
+
+void on_check_ping_toggled( GtkToggleButton *tb,
+			    gpointer user_data )
+{
+	if( gtk_toggle_button_get_active( tb ) ) {
+		printf("Pinging enabled\n");
+	} else {
+		printf("Pinging disabled\n");
+	}
 }
