344bde558e5eeb2cae10cfafa478d95a6b3142c6
psmith <psmith@98f67ba8-b320-0410-bda0-b753e294843c>
2008-06-11 11:05:15 +0000

    Added check for when board powers down

--- slug/utils/battmon/battmon.c
+++ slug/utils/battmon/battmon.c
@@ -14,14 +14,23 @@
 #define ADDRESS 0x3f
 
 /* Reads a word from the I2C slave */
-uint16_t readword(int fd, uint8_t cmd);
+uint16_t readword(int fd, uint8_t cmd, FILE* write_file);
 
 int main (int argc, char **argv)
 {
 
-
-
   int fd = 0;
+
+  int len; 			/* Defines the number of seconds to execute for */
+  
+  if (argc < 2)
+    {
+      fprintf (stderr, "Incorrect number of arguments:\n\n");
+      fprintf (stderr, "./battmon <number of seconds to execute>\n");
+      exit (0);
+    }
+  else
+    len = atoi (argv[1]);
 
   /* Attempt to open I2C device (/dev/i2c-0) */
   fd = open ("/dev/i2c-0", O_RDWR);
@@ -53,7 +62,18 @@ int main (int argc, char **argv)
 
   uint16_t current;		/* The relative 16bit Current Value */
   uint16_t voltage;		/* The relative 16bit Voltage Value */
-  
+
+  int i = 0; 			/* Loop variable */
+
+  FILE *write_file = NULL;	/* Output file pointer */
+
+  /* Attempt to open output file */
+  if ((write_file = fopen ("output", "w")) == NULL)
+    {
+      fprintf (stderr, "Error opening file!");
+      exit (0);
+    }
+
   /* Set the epoch value */
   gettimeofday (&tp, tpz);
   epoch = tp.tv_sec;  
@@ -61,21 +81,23 @@ int main (int argc, char **argv)
   /* Begin Current and Voltage Reading */
   while (1)
     {
+      fprintf (stderr, "Current iteration: %d\n", i);
+      
       /* Read current and voltage from I2C */
-      voltage = readword (fd, GETV);
-      current = readword (fd, GETI);      
+      voltage = readword (fd, GETV, write_file);
+      current = readword (fd, GETI, write_file);      
       
       /* Get the current time */
       gettimeofday (&tp, tpz);
       currenttime = tp.tv_sec;
 
       /* Output the current time relative to epoch */
-      fprintf (stderr, "Voltage: %u, Time: %d\n", voltage, currenttime - epoch);
-      fprintf (stderr, "Current: %u, Time: %d\n", current, currenttime - epoch);
+      fprintf (write_file, "%u, %u, %d\n", voltage, current, (currenttime - epoch));
       sleep (1);
+      i++;
     }
 
-
+  fclose (write_file);
   return 0;
 
 }
@@ -84,17 +106,32 @@ int main (int argc, char **argv)
 /* Read a word from the I2C */
 /* GETV - Gets a value for the voltage */
 /* GETI - Gets a value for the current */
-uint16_t readword(int fd, uint8_t cmd)
+uint16_t readword(int fd, uint8_t cmd, FILE *write_file)
 {
   signed int tmp = -1;
+  int i = 0;
 
   /* Attempt to read from I2C */
   while (tmp < 1)
     {
       tmp = i2c_smbus_read_word_data( fd, cmd );
       if (tmp < 1)
-	fprintf (stderr, "Error Reading from I2C Bus: %d\n", cmd);
+	{
+	  while (i<5 && tmp < 1)
+	    {
+	      fprintf (stderr, "Error Reading from I2C Bus: %d\n", cmd);
+	      tmp = i2c_smbus_read_word_data(fd, cmd);
+	      i++;
+	    }
+	  if (tmp < 1)
+	    {
+	      fprintf (stderr, "Power Lost to Power Board\n");
+	      fclose (write_file);
+	      exit (0);
+	    }
+	  else
+	    return tmp;
+	}
     }
-
   return tmp;
 }
