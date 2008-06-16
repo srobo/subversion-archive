4abe8dd9fc220fb338c41b6e48764599ee8547f4
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2008-06-16 17:31:39 +0000

    improved info in false db

--- ide2/task_list.inc.php
+++ ide2/task_list.inc.php
@@ -1,29 +1,31 @@
 <?php
-$task_list	= array();
+$ide_sect	= array();
 
 $ide_sect[0]['id']	= 1;
-$ide_sect[0]['title']	= "Fix Power Board Resistor";
-$ide_sect[0]['description']	= "Solder a wire across the sense resistor.";
+$ide_sect[0]['title']	= "Checklist";
+$ide_sect[0]['link']	= "Checklist.php";
+$ide_sect[0]['description']	= "This checklist allows easy progress tracking throught the year.";
 $ide_sect[0]['related_docs_help']	= "";
-$ide_sect[0]['teams_comments']	= "";
-$ide_sect[0]['mentor_comments']	= "";
-$ide_sect[0]['category']	= "Electronics";
+$ide_sect[0]['teams_comments']	= "Please provide feedback here.";
+$ide_sect[0]['mentor_comments']	= "Please provide feedback here.";
 
 $ide_sect[1]['id']	= 2;
-$ide_sect[1]['title']	= "Wire Up Boards";
-$ide_sect[1]['description']	= "Connect up with RJ11 cables.";
+$ide_sect[1]['title']	= "Progress Graphs";
+$ide_sect[1]['link']	= "Graphs.php";
+$ide_sect[1]['description']	= "Show progress information in an easily accessible form.";
 $ide_sect[1]['related_docs_help']	= "";
-$ide_sect[1]['teams_comments']	= "";
-$ide_sect[1]['mentor_comments']	= "";
-$ide_sect[1]['category']	= "Electronics";
+$ide_sect[1]['teams_comments']	= "Please provide feedback here.";
+$ide_sect[1]['mentor_comments']	= "Please provide feedback here.";
 
-$ide_sect[2]['id']	= 12;
-$ide_sect[2]['title']	= "Order Motors";
-$ide_sect[2]['description']	= " Decide upon the specification of the motors you will need for your robot and order them from your chosen manufacturer.";
+$ide_sect[2]['id']	= 3;
+$ide_sect[2]['title']	= "Documentation";
+$ide_sect[2]['link']	= "Documentation.php";
+$ide_sect[2]['description']	= " Contains up-to-date links to both our documentation and external information we think you'll find useful.";
 $ide_sect[2]['related_docs_help']	= "*[[http://en.wikipedia.org/wiki/Torque|Torque]] - What is it?\n*The Motor Controller Board\n*[[http://www.maplin.co.uk/|Maplins]] - featured component\n*[[http://uk.farnell.com/|Farnell]] - featured component\n*[[http://uk.rs-online.com/web/|RS]] - featured component";
-$ide_sect[2]['teams_comments']	= "We've got the best motors!!";
-$ide_sect[2]['mentor_comments']	= "Expensive but should do the job!";
-$ide_sect[2]['category']	= "Mechanical";
+$ide_sect[2]['teams_comments']	= "Please provide feedback here.";
+$ide_sect[2]['mentor_comments']	= "Please provide feedback here.";
+
+$task_list	= array();
 
 $task_list[0]['id']	= 1;
 $task_list[0]['title']	= "Fix Power Board Resistor";
@@ -89,6 +91,8 @@ $task_list[4]['signoff_date']	= 0;
 $task_list[4]['completed']	= 0;
 $task_list[4]['category']	= "Electronics";
 $task_list[4]['deadline']	= "2008-07-11";
+
+$teams_completed	= array();
 
 $teams_completed[1]	= 4;
 $teams_completed[2]	= 6;
