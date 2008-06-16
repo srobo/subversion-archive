4de704cc14805832420835c5de960f05154b84a8
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2008-06-16 21:43:04 +0000

    ajaxy files

---
+++ ide2/ajax/scripts/login.js
@@ -0,0 +1,20 @@
+window.addEvent('domready', function() {
+	var morph = new Fx.Morph('navigate');
+	var morph2 = new Fx.Morph('top-spacer');
+	
+	var navigate = $('navigate');
+	var top = $('top-spacer');
+
+	$('trigger').addEvent('click', function() {
+		morph.start({
+			width: '300px',
+			display: 'block'
+		}).chain(function(){
+			// executes immediately after completion of above effect
+			this.start({
+				width: '0px'
+			});
+		});
+	});
+});
+
