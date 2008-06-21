d7d87b0b6b15fc02f2859693035af46aab8b9980
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2008-06-21 00:22:31 +0000

    loading image, login event

--- ide2/ajax/scripts/login.js
+++ ide2/ajax/scripts/login.js
@@ -1,25 +1,27 @@
-window.addEvent('domready', function() {
+window.addEvent('domready', function() {	//var mylogingif = new Asset.image('srobo-login.gif', {id: 'login-img', title: 'ALoginGif'});
 	var morph = new Fx.Morph('navigate');
 	var morph2 = new Fx.Morph('top-spacer');
 	
 	var navigate = $('navigate');
 	var top = $('top-spacer');
 
+
 	var scene2 = function(){
 			morph.start.delay(2000, this, {
-				width: '0px'});
-			};
-	var scene3 = function(){
-			morph.start({
-				display: 'none'});
+				width: '0px',
+				opacity: 0});
 			};
 
 	var scene1 = function(){ 
 			morph.start({
 				width: '200px',
-				display: 'block'}).chain(scene2).chain(scene3);
+				display: 'block',
+				opacity: 1}).chain(scene2);
+			top.set('html', '<img src=\'file:///home/chris/srobo/ide2/ajax/images/srobo-login.gif\' /> Logging you in...');
+			(function(){top.set('html', 'You are now logged in')}).delay(2000, this);
 			};
 
 	$('trigger').addEvent('click', scene1);
+	$('login').addEvent('submit', scene1);
 });
 
