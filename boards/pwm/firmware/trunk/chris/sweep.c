f04b86ab9a7b2d6ac858da3228a3d9d5a4ed89f6
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2008-08-18 15:37:23 +0000

    Merged pwm and motor code to upgrade PWM Board Firmware ready for larger MSP430 chip

---
+++ boards/pwm/firmware/trunk/chris/sweep.c
@@ -0,0 +1,46 @@
+#include "sweep.h"
+#include <stdint.h>
+#include "hardware.h"
+#include "servo.h"
+
+/**
+Delay function, will run a while loop corresponding to 1ms when FOSC is 12Mhz
+@param uint32 number of ms the delay must last
+TODO -  possibly investigate a bug that increments the duration of the delay
+inexplicably when a value greater than 30000 is given to i.
+*/
+void delay_MS(uint32_t d)
+{
+	volatile uint16_t i;
+
+	while(d--)
+	{
+		i = 15000;
+		while(i>0) 
+			i--;
+
+		i = 20000;
+		while(i>0)
+			i--;
+	}
+}
+
+/* This function sweeps all servos between two hardcoded arbitrary positions */
+#if(SWEEP_SERVOS)
+void sweepServo(void)
+{
+	uint8_t channel;
+
+	delay_MS(50);
+	for( channel=0; channel < SERVO_NUMBER; channel++ )
+		servo_set_pwm(channel, (uint16_t)(2.3*TICKS_PER_MS));
+
+	delay_MS(50);
+	for( channel=0; channel < SERVO_NUMBER; channel++ )
+		servo_set_pwm(channel, (uint16_t)(0.7*TICKS_PER_MS));
+}
+#else
+void sweepServo(void)
+{
+}
+#endif
