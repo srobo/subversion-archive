56e2e6c2fa27b46ec004309413a4c21d96581b51
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2008-09-14 01:10:25 +0000

    roboide: Move the projects page file listing stuff into its own class: ProjFileList

--- Robo-IDE/roboide/static/javascript/projpage.js
+++ Robo-IDE/roboide/static/javascript/projpage.js
@@ -1,16 +1,18 @@
 // The project page
 function ProjPage() {
 	this._initted = false;
-
 	this._ps_prompt = null;
+	this.flist = null;
 }
 
 // Initialise the project page -- but don't show it
 ProjPage.prototype._init = function() {
 	if( this._initted )
 		return;
-	
+
 	MochiKit.Signal.connect( "project-select", "onchange", bind(this._list_changed, this) );
+
+	this.flist = new ProjFileList();
 	this._initted = true;
 }
 
@@ -22,7 +24,7 @@ ProjPage.prototype.show = function() {
 	
 	// If we have a project setup
 	if (project != "")
-		this._flist();
+		this.flist.update();
 	
 	// Load/refresh the projects list
 	this._populate_list();
@@ -38,7 +40,7 @@ ProjPage.prototype.hide = function() {
 ProjPage.prototype.change_project = function(proj) {
 	project = proj;
 	
-	this._flist();
+	this.flist.update();
 }
 
 // Retrieves a list of projects and populates the project selection list
@@ -100,32 +102,6 @@ ProjPage.prototype.rpane_show = function() {
 }
 
 // ***** Project Page File Listing *****
-// Request and update the project file listing
-ProjPage.prototype._flist = function() {
-	var d = MochiKit.Async.loadJSONDoc("./filelist", {team : 1,
-		rootpath : project});
-	
-	d.addCallback( bind( this._flist_received, this ) );
-	
-	d.addErrback( bind( function (){
-		status_button( "Error getting the file list", LEVEL_ERROR,
-			       "retry", bind( this._flist, this ) );
-	}, this ) );
-}
-
-ProjPage.prototype._flist_received = function(nodes) {
-	log( "filelist received" );
-	
-	MochiKit.DOM.swapDOM( "proj-filelist",
-			      MochiKit.DOM.UL( { "id" : "proj-filelist",
-						 "style" : "display:none" },
-					       map_1( bind(this._flist_dir, this), 0, nodes["tree"] ) ) );
-	
-	MochiKit.DOM.getElement( "proj-name" ).innerHTML = "Project " + project;
-	
-	projpage.rpane_show();
-}
-
 function map_1( func, arg, arr ) {
 	var a = function( item ) {
 		return func( item, arg );
@@ -134,24 +110,56 @@ function map_1( func, arg, arr ) {
 	return MochiKit.Base.map( a, arr );
 }
 
+// Project page file list
+function ProjFileList() {
+	
+}
+
+// Request and update the project file listing
+ProjFileList.prototype.update = function() {
+	var d = MochiKit.Async.loadJSONDoc("./filelist", {team : 1,
+		rootpath : project});
+	
+	d.addCallback( bind( this._received, this ) );
+	
+	d.addErrback( bind( function (){
+		status_button( "Error getting the file list", LEVEL_ERROR,
+			       "retry", bind( this.update, this ) );
+	}, this ) );
+}
+
+ProjFileList.prototype._received = function(nodes) {
+	log( "filelist received" );
+	
+	MochiKit.DOM.swapDOM( "proj-filelist",
+			      MochiKit.DOM.UL( { "id" : "proj-filelist",
+						 "style" : "display:none" },
+					       map_1( bind(this._dir, this), 0, nodes["tree"] ) ) );
+	
+	MochiKit.DOM.getElement( "proj-name" ).innerHTML = "Project " + project;
+	
+	projpage.rpane_show();
+}
+
+
 // Produce an object consisted of "level" levels of nested divs
 // the final div contains the DOM object inner 
-ProjPage.prototype._flist_nested_divs = function( level, inner ) {
+ProjFileList.prototype._nested_divs = function( level, inner ) {
 	if (level == 0)
 		return inner;
 	
 	if (level > 1)
-		return MochiKit.DOM.DIV( null, this._flist_nested_divs( level-1, inner ) );
+		return MochiKit.DOM.DIV( null, this._nested_divs( level-1, inner ) );
 	
 	return MochiKit.DOM.DIV( null, inner );
 }
 
 // Returns a DOM object for the given node 
-ProjPage.prototype._flist_dir = function( node, level ) {
+ProjFileList.prototype._dir = function( node, level ) {
 	// Assemble the link with divs in it
 	var link = MochiKit.DOM.A( { "href" : "#" },
-		this._flist_nested_divs( level, node.name + (node.kind == "FOLDER"?"/":"") ) );
-	MochiKit.Signal.connect( link, "onclick", bind( this._flist_onclick, this ) );
+		this._nested_divs( level, node.name + (node.kind == "FOLDER"?"/":"") ) );
+	MochiKit.Signal.connect( link, "onclick", bind( this._onclick, this ) );
 	
 	if( node.kind == "FILE" ) {
 		var n = MochiKit.DOM.LI( null, link );
@@ -159,12 +167,12 @@ ProjPage.prototype._flist_dir = function( node, level ) {
 	} else
 		var n = MochiKit.DOM.LI( null, [ link, 
 	    		MochiKit.DOM.UL( { "class" : "flist-l" }, 
-			map_1( bind(this._flist_dir, this), level + 1, node["children"] ) ) ] );
+			map_1( bind(this._dir, this), level + 1, node["children"] ) ) ] );
 	return n;
 }
 
 // The onclick event for the filelist items
-ProjPage.prototype._flist_onclick = function(ev) {
+ProjFileList.prototype._onclick = function(ev) {
 	// Prevent the browser doing something when someone clicks on this
 	ev.preventDefault();
 	ev.stopPropagation();
@@ -173,3 +181,4 @@ ProjPage.prototype._flist_onclick = function(ev) {
 	if( mods["ctrl"] )
 		MochiKit.DOM.toggleElementClass( "selected", ev.src() );
 }
+
