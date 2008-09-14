295cc52f97b9f8e632c7273d5bd6c2f59f487db9
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2008-09-14 00:16:49 +0000

    removed login tab - if this needs to be called, use firebug to call tabChange(2)
    active tab (tab has focus) now changes color to notify user
    
    fixed onclick event problems using MochiKit.Base.bind()

--- Robo-IDE/roboide/static/javascript/gui.js
+++ Robo-IDE/roboide/static/javascript/gui.js
@@ -106,19 +106,17 @@ MochiKit.DOM.addLoadEvent( function() {
 	//Projects Tab
 	tab_manager.addTab({'isPerm' : true, 
 						'label' : 'Projects', 
-						'onClick' : function(){tabChange(1)} });
-	//Login Tab
-	tab_manager.addTab({'isPerm' : true, 
-						'label' : 'Login', 
-						'onClick' : function(){tabChange(2)} });
+						'onClick' : function(){
+										projpage.show();
+										MochiKit.Style.setStyle('edit-mode', {'display':'none'});
+										} });
+
 	//+New Tab
 	tab_manager.addTab({'isPerm' : true, 
 						'label' : '+ New +', 
-						'onClick' : function(){
-							tab_manager.addTab({'isPerm' : false, 
-							'label' : 'new.py', 
-							'onClick' : null})}
-						 });
+						'onClick' : openNewTab});
+	//Open project Tab
+	tab_manager.changeTab("Projects");
 	//Initialise the editarea component.
 // 	editAreaLoader.init({
 // 		id : "codebox",
@@ -858,58 +856,3 @@ function startLogin(username, password) {
 	d.addCallbacks(gotMetadata, failMetadata);
 }
 
-//class defines a single Tab within the Tab Bar
-function Tab(Tbar, args){
-	this.properties = args;
-
-	//{'label' : string, 		the text visible in the tab
-	// 'onClick' : function object, 		the function called when tab is clicked
-	// 'isPerm' : bool}		if true, tab cannot be removed - i.e. Project tab must always be visible
-
-	this.draw = function(Tbar) {		
-		var myLink = MochiKit.DOM.A({"href" : "#"});
-		myLink.innerHTML = this.properties.label;
-		var myList = MochiKit.DOM.LI(null, "");
-		MochiKit.DOM.appendChildNodes(myList, myLink);		
-		MochiKit.DOM.appendChildNodes(Tbar, myList);
-		MochiKit.Signal.connect(myList, 'onclick', this.properties.onClick);
-		return myList;
-	}
-	
-	this.handle = this.draw(Tbar);
-
-	this.close = function() {
-		MochiKit.DOM.removeElement(this.handle);
-	}
-}
-
-//Tab manager instigates creation  and deletion of tabs
-var tab_manager = new function () {
-	this.TabList = new Array();
-
-	this.init = function() {
-		this.TabList = new Array();
-	}
-	
-	this.addTab = function(args) {
-		this.TabList[this.TabList.length] = new Tab(MochiKit.DOM.getElement("tab-list"), args);
-	}
-
-	this.closeTab = function(tname) {
-		for (i = 0; i < this.TabList.length; i++) {
-			if((this.TabList[i].properties.label == tname) && !(this.TabList[i].properties.isPerm)) {
-				this.TabList[i].close();
-				this.TabList.splice(i, 1);
-				break;
-			}
-		}
-	}
-	
-	this.closeAll = function() {
-		for (i = 0; i < this.TabList.length; i++) {
-			if(!this.TabList[i].properties.isPerm)
-				this.TabList[i].close();	
-		}	
-		this.init();
-	}
-}();
--- Robo-IDE/roboide/static/javascript/tabs.js
+++ Robo-IDE/roboide/static/javascript/tabs.js
@@ -1,22 +1,55 @@
+//event handler for new file
+function openNewTab(){
+	var newTabName = 'New'+Math.round((Math.random()*100))+'.py';
+	tab_manager.addTab({'isPerm' : true, 
+						'label' : newTabName, 
+						'onClick' : MochiKit.Base.bind("changeTab", tab_manager, newTabName)});
+}
+
 //class defines a single Tab within the Tab Bar
 function Tab(Tbar, args){
 	this.properties = args;
+	this.tabHandle;
+	this.linkHandle
 
-	//{'label' : string, 		the text visible in the tab
-	// 'onClick' : function object, 		the function called when tab is clicked
-	// 'isPerm' : bool}		if true, tab cannot be removed - i.e. Project tab must always be visible
-
-	this.draw = function(Tbar) {		
-		var myLink = MochiKit.DOM.A({"href" : "#"});
-		myLink.innerHTML = this.properties.label;
+	//{'label' : string, 				the text visible in the tab
+	// 'onClick' : function object, 	the function called when tab is clicked
+	// 'isPerm' : bool					if true, tab cannot be removed - i.e. Project tab must always be visible
+	// 'focus' : bool}					if true, tab is on top
+	
+	this.properties.focus = true;
+		
+	this.draw = function(Tbar) {	
+		if(	this.properties.focus ) {
+			this.linkHandle = MochiKit.DOM.A({"href" : "#", "class" : "focus"}); 
+		}
+		else{
+			this.linkHandle = MochiKit.DOM.A({"href" : "#", "class" : "nofocus"});
+		}			
+		this.linkHandle.innerHTML = this.properties.label;
 		var myList = MochiKit.DOM.LI(null, "");
-		MochiKit.DOM.appendChildNodes(myList, myLink);		
+		MochiKit.DOM.appendChildNodes(myList, this.linkHandle);		
 		MochiKit.DOM.appendChildNodes(Tbar, myList);
 		MochiKit.Signal.connect(myList, 'onclick', this.properties.onClick);
+		
+		MochiKit.Do		
+
 		return myList;
 	}
 	
 	this.handle = this.draw(Tbar);
+	
+	this.hasFocus = function(opt) {
+		if(opt){		
+			MochiKit.DOM.setElementClass(this.linkHandle, "focus")
+			this.properties.focus	=  true;
+		}
+		else{
+			MochiKit.DOM.setElementClass(this.linkHandle, "nofocus")
+			this.properties.focus	=  false;		
+			}
+
+	}
 
 	this.close = function() {
 		MochiKit.DOM.removeElement(this.handle);
@@ -33,7 +66,9 @@ var tab_manager = new function () {
 	
 	this.addTab = function(args) {
 		this.TabList[this.TabList.length] = new Tab(MochiKit.DOM.getElement("tab-list"), args);
+		this.changeTab(args.label);
 	}
+
 
 	this.closeTab = function(tname) {
 		for (i = 0; i < this.TabList.length; i++) {
@@ -45,6 +80,17 @@ var tab_manager = new function () {
 		}
 	}
 	
+	this.changeTab = function(tab){
+		for (i = 0; i < this.TabList.length; i++) {
+			if(this.TabList[i].properties.label == tab) {
+				this.TabList[i].hasFocus(true);
+			}
+			else{
+				this.TabList[i].hasFocus(false);
+			}
+		}
+	}
+
 	this.closeAll = function() {
 		for (i = 0; i < this.TabList.length; i++) {
 			if(!this.TabList[i].properties.isPerm)
@@ -52,4 +98,5 @@ var tab_manager = new function () {
 		}	
 		this.init();
 	}
+
 }();
