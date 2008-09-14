e403b92203bd5aaf035aa18eb9620a948310b995
ccross <ccross@98f67ba8-b320-0410-bda0-b753e294843c>
2008-09-14 02:29:40 +0000

    Moved from tab_manager approach to just one class to describe a single tab. all the necessary functions are encapsulated within that one class. A global array maintains the tabs currently loaded

--- Robo-IDE/roboide/static/javascript/tabs.js
+++ Robo-IDE/roboide/static/javascript/tabs.js
@@ -1,106 +1,59 @@
 //event handler for new file
 function openNewTab(){
 	var newTabName = 'New'+Math.round((Math.random()*100))+'.py';
-	tab_manager.addTab({'isPerm' : true, 
-						'label' : newTabName, 
-						'onClick' : MochiKit.Base.bind("changeTab", tab_manager, newTabName)});
+	TABLIST.push(new Tab($("tab-list"), {'isPerm' : false, 
+										'label' : newTabName, 
+										'onclick' : null}));
 }
 
 //class defines a single Tab within the Tab Bar
 function Tab(Tbar, args){
 	this.properties = args;
-	this.tabHandle;
-	this.linkHandle
+	this.tabHandle = null;
+	this.linkHandle = null;
+	this.properties.focus = true;
 
 	//{'label' : string, 				the text visible in the tab
-	// 'onClick' : function object, 	the function called when tab is clicked
+	// 'project' : project name			the project to which the file in tab belongs
+	// 'onclick' : function object, 	the function called when tab is clicked
 	// 'isPerm' : bool					if true, tab cannot be removed - i.e. Project tab must always be visible
-	// 'focus' : bool}					if true, tab is on top
-	
-	this.properties.focus = true;
+	// 'focus' : bool}					if true, tab is on top	
 		
-	this.draw = function(Tbar) {	
-		if(	this.properties.focus ) {
-			this.linkHandle = MochiKit.DOM.A({"href" : "#", "class" : "focus"}); 
-		}
-		else{
-			this.linkHandle = MochiKit.DOM.A({"href" : "#", "class" : "nofocus"});
-		}			
+	this.open = function(Tbar) {	
+		this.linkHandle = MochiKit.DOM.A({"href" : "#", "class" : "focus"}); 	
 		this.linkHandle.innerHTML = this.properties.label;
-		var myList = MochiKit.DOM.LI(null, "");
-		MochiKit.DOM.appendChildNodes(myList, this.linkHandle);		
-		MochiKit.DOM.appendChildNodes(Tbar, myList);
-		MochiKit.Signal.connect(myList, 'onclick', this.properties.onClick);	
+		this.tabHandle = MochiKit.DOM.LI(null, "");
+		MochiKit.DOM.appendChildNodes(this.tabHandle, this.linkHandle);		
+		MochiKit.DOM.appendChildNodes(Tbar, this.tabHandle);
 
-		return myList;
+		if(this.properties.onclick) {
+			MochiKit.Signal.connect(this.tabHandle, 'onclick', this.properties.onclick);	
+		}
+		return this;
 	}
 	
-	this.handle = this.draw(Tbar);
+	this.open(Tbar);	
 	
-	this.hasFocus = function(opt) {
-		if(opt){		
-			MochiKit.DOM.setElementClass(this.linkHandle, "focus")
-			this.properties.focus	=  true;
-		}
-		else{
-			MochiKit.DOM.setElementClass(this.linkHandle, "nofocus")
-			this.properties.focus	=  false;		
-			}
-
+	this.gainFocus = function() {
+	
+		MochiKit.DOM.setElementClass(this.linkHandle, "focus")
+		this.properties.focus	=  true;
+	}
+	this.loseFocus = function() {
+	
+		MochiKit.DOM.setElementClass(this.linkHandle, "nofocus")
+		this.properties.focus	=  false;
 	}
 
 	this.close = function() {
-		MochiKit.DOM.removeElement(this.handle);
+		MochiKit.DOM.removeElement(this.tabHandle);
+	}
+	
+	this.flash = function() {
+		var effect = new Highlight(this.linkHandle);
+		effect.options.startcolor = '#253571';
+		effect.options.endcolor = '#2ee5e1';
+		effect.start();
 	}
 }
 
-//Tab manager instigates creation  and deletion of tabs
-var tab_manager = new function () {
-	this.TabList = new Array();
-
-	this.init = function() {
-		this.TabList = new Array();
-	}
-	
-	this.addTab = function(args) {
-		this.TabList[this.TabList.length] = new Tab(MochiKit.DOM.getElement("tab-list"), args);
-		this.changeTab(args.label);
-	}
-
-
-	this.closeTab = function(tname) {
-		for (i = 0; i < this.TabList.length; i++) {
-			if((this.TabList[i].properties.label == tname) && !(this.TabList[i].properties.isPerm)) {
-				this.TabList[i].close();
-				this.TabList.splice(i, 1);
-				break;
-			}
-		}
-	}
-	
-	this.changeTab = function(tab){
-		if(tab == "Projects") { 
-			MochiKit.Style.setStyle('edit-mode', {'display':'none'});
-			projpage.show();}
-		else{
-			MochiKit.Style.setStyle('edit-mode', {'display':'block'});
-			projpage.hide();}
-		for (i = 0; i < this.TabList.length; i++) {						
-			if(this.TabList[i].properties.label == tab) {
-				this.TabList[i].hasFocus(true);
-			}
-			else{
-				this.TabList[i].hasFocus(false);
-			}
-		}
-	}
-
-	this.closeAll = function() {
-		for (i = 0; i < this.TabList.length; i++) {
-			if(!this.TabList[i].properties.isPerm)
-				this.TabList[i].close();	
-		}	
-		this.init();
-	}
-
-}();
