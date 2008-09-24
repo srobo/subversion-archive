ced107c126b5f3aaa1d1bbc4b602856f8402ac22
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2008-09-24 23:41:24 +0000

    user-cards: Include information in the cards.

--- web/user-cards/make-cards.py
+++ web/user-cards/make-cards.py
@@ -24,39 +24,84 @@ def header():
 \\pagestyle{empty} % removes page numbers
 \\begin{document}
 \\noindent
-\\setlength{\\unitlength}{1in}
-\\begin{picture}(8.5,11)(0.0,0.3937) % revised for 5 vertical per page
-  \\thicklines
-"""
+\\setlength{\\unitlength}{1in}"""
 
+def page_header():
+    print """\\begin{picture}(8.5,11)(0.0,0.3937) % revised for 5 vertical per page
+  \\thicklines """
+
+def page_footer():
+    print """\\end{picture}\clearpage"""
 
 def footer():
-    print """\\end{picture}
-\\end{document}
-"""
+    print """\\end{document}"""
 
-def output(x,y,contents):
+def output(x,y,user):
     print "\\put(%f,%f) {" % ( x * X_DELTA,
                               y * Y_DELTA )
 
-    print """\\framebox(3.5,2){ % x,y size of box, inches
-      \\shortstack[l]{"""
-    print contents
-    print """} % end shortstack
-    } % end makebox
+    print """\\framebox(3.5,2)[c]{"""
+    print """\\shortstack[l]{"""
+    print "\\textbf{Student Robotics Login Details}\\\\"
+    print "\\vspace{3pt} \\\\"
+
+    print "%s %s\\\\" % ( user["fname"], user["lname"] )
+    print "Username: \\texttt{%s} \\\\" % user["username"]
+    print "Password: \\texttt{%s}" % user["password"]
+    print "\\vspace{3pt} \\\\"
+    print "Use these details on the Student Robotics website: \\\\"
+    print "\\texttt{http://www.studentrobotics.org/}"
+    print "\\vspace{6pt} \\\\"
+    print "Where you will find the IDE, forums, "
+    print "tutorials and \\\\ documentation, and the  "
+    print "ability to change your \\\\ password."
+
+    print """} % end shortstack"""
+    print """} % end makebox
   }"""
 
 users = []
 
-users.append( {"username":"rspanton",
+users.append( {"fname": "Robert",
+               "lname": "Spanton",
+               "username":"rspanton",
                "password":"dfhgdfgo",
+               "email":"rspanton@zepler.net"} )
+users.append( { "fname" : "Tom",
+                "lname" : "Bennellick",
+                "username":"tbennellick",
+               "password":"0hjsd-0js",
                "email":""} )
+for x in range(0,12):
+    users.append( { "fname" : "Chris",
+                    "lname" : "Cross",
+                    "username":"ccross",
+                   "password":"er0lngldsp",
+                   "email":""} )
 
 header()
 
-for x in [0,1]:
-    for y in xrange(0,4):
-        output(x,y,"badger")
+x = 0
+y = 0
+COLS = 2
+ROWS = 5
+
+for user in users:
+    if x == 0 and y == 0:
+        page_header()
+
+    output(x,y,user)
+
+    x = x + 1
+    if x == COLS:
+        x = 0
+        y = y + 1
+
+        if y == ROWS:
+            y = 0
+            page_footer()
+
+page_footer()
 
 
 footer()
