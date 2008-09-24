84eb7ed72e80d64b51c266d163fb403c3de2f50b
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2008-09-24 21:30:04 +0000

    makegerb: now selects eagle version - discovered irellevant as eagle 5 can do e4 boards

--- boards/prototype/makegerb.sh
+++ boards/prototype/makegerb.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 
-
-EXPECTED_ARGS=2
-#EXTENDED_ARGS=3
+EXPECTED_ARGS=3
 E_BADARGS=65
-
 
 
 
 if [ $# -lt $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` <file.brd>"
+  echo "Usage: `basename $0` [4/5][file.brd] <xshift  yshift>"
   echo "output files in gerbers/"
   exit $E_BADARGS
 fi
 
 
+if [ $1 -eq "5" ]
+then
+    egl="/opt/eagle-5.2.0/bin/eagle"
+    echo 'using egl 5'
+
+elif [ $1 -eq "4" ]
+then
+    egl="/opt/eagle/bin/eagle"
+    echo 'using egl 5'
+else
+    echo "error second argument must be 4 or 5 indicating eagle version to use"
+    exit 
+fi
+
+
+
 if [ $# -gt $EXPECTED_ARGS ]
 then
-    of1="-x$3 -y$4"
+    of1="-x$4 -y$5"
 else
     of1=""
 fi
 echo $of1
+echo $egl
 
-
-eagle -X -dGERBER_RS274X  $of1 -o$2-top.gbr $1 top pad via
-eagle -X -dGERBER_RS274X  $of1 -o$2-bottom.gbr $1 bottom pad via
-eagle -X -dGERBER_RS274X  $of1 -o$2-dim.gbr $1 dimension
-eagle -X -dexcellon  $of1 -o$2-drill.drd $1 drills
+$egl -X -dGERBER_RS274X  $of1 -o$3-top.gbr $2 top pad via
+$egl -X -dGERBER_RS274X  $of1 -o$3-bottom.gbr $2 bottom pad via
+$egl -X -dGERBER_RS274X  $of1 -o$3-dim.gbr $2 dimension
+$egl -X -dexcellon  $of1 -o$3-drill.drd $2 drills
     
-
 
 #eagle -X -dGERBER_RS274X -x3 -y4 -o$2-topshift.gbr $1 top pad via
 
