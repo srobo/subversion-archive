75365b10aa4425293648b744d4f3c557c494f43c
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2008-11-11 00:32:03 +0000

    updated resistor values for new leds

---
+++ boards/prototype/lock.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+svn lock ../power/pcb/trunk/PWR1.{brd,sch}
+svn lock ../motor/pcb/trunk/motor.{brd,sch}
+svn lock ../jointio/pcb/trunk/jointio.{sch,brd}
+svn lock ../pwm/pcb/trunk/pwm-board.{sch,brd}
