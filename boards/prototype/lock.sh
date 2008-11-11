54855d8b7b6402fa439d46981a2fc6c7aa6d0bd2
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2008-11-11 14:37:29 +0000

    farnell order completed

--- boards/prototype/lock.sh
+++ boards/prototype/lock.sh
@@ -2,4 +2,5 @@
 svn lock ../power/pcb/trunk/PWR1.{brd,sch}
 svn lock ../motor/pcb/trunk/motor.{brd,sch}
 svn lock ../jointio/pcb/trunk/jointio.{sch,brd}
-svn lock ../pwm/pcb/trunk/pwm-board.{sch,brd}
+svn lock ../pwm/pcb/trunk/pwm-board.{sch,brd} 
+svn lock parts/big-bom-08.ods 
