dfa345303a2ccf9e858c8458033830f6b4ccfa51
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2008-11-21 23:49:24 +0000

    Remove "ide2".  This was an experiment into the IDE theme.

--- ide2/Checklist.php
+++
@@ -1,7 +0,0 @@
-<?php
-$external_scripts	= "Checklist_html.js";
-
-include 'Head.inc.php';
-include 'get_checklist_html.php';
-include 'Foot.inc.php';
-?>
--- ide2/ajax/scripts/demo.js
+++
@@ -1,12 +0,0 @@
-window.addEvent('domready', function() {	
-	alert("hello");
-	/*
-	var morph = new Fx.Morph('anotherElement');
-	
-	$('morphEffect').addEvent('click', function(e) {
-		e.stop();
-		morph.start({
-			color: '#C6D880'
-		});
-	});	*/
-});
