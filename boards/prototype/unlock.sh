c08f1186ef14aaa16ecbabc55eb452743d285c1a
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-02 01:18:22 +0000

    added lock removal script

---
+++ boards/prototype/unlock.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+svn unlock ../power/pcb/trunk/PWR1.{brd,sch}
+svn unlock ../motor/pcb/trunk/motor.{brd,sch}
+svn unlock ../jointio/pcb/trunk/jointio.{sch,brd}
+svn unlock ../pwm/pcb/trunk/pwm-board.{sch,brd} 
+svn unlock parts/big-bom-08.ods 
