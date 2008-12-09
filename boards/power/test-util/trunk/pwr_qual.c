5e63538a1d5ace4fbbecfcd7d49b251bb4c6ffa5
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-09 23:29:18 +0000

    power b oard i2c write works to leds

--- boards/power/test-util/trunk/pwr_qual.c
+++ boards/power/test-util/trunk/pwr_qual.c
@@ -10,6 +10,162 @@
 #include <time.h>
 #include "pwr_qual.h"
 
+int init_i2c(void){
+	int fd;			
+      
+	fd = open( "/dev/i2c-1", O_RDWR );
+	
+	if( fd == -1 )
+	{
+		fprintf( stderr, "Failed to open /dev/`i2c-1: %m\n" );
+		return 1;
+	}
+
+	
+
+	if( ioctl( fd, I2C_SLAVE, ADDRESS ) < 0 )
+	{
+		fprintf( stderr, "Failed to set slave address: %m\n" );
+		return 2;
+	}
+
+	
+
+	if( ioctl( fd, I2C_PEC, 1) < 0)
+	{
+		fprintf( stderr, "Failed to enable PEC\n");
+		return 3;
+	}
+
+	return fd;
+}
+
+
+int main( int argc, char** argv )
+{
+	int fd =0;
+	int retval;
+	uint8_t value[30];
+	
+	fd = init_i2c();
+	value[0] = 0xff;
+	value[1] = 0xff;
+	retval = i2c_smbus_write_block_data(fd,1,1,value);
+	printf("returned %d",retval);
+	return;
+
+}
+
+/* len = i2c_smbus_read_byte_data( gum->fd, reg ); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 	//------------------------------ */
+/* 	if (argc<2){ // check at least 1 arg */
+/* 		printf("incorrect args\n"); */
+/* 		//printf("Usage: %s {w,l,v,i,r,s}\n"); */
+/* 		printf("w - identify(currently unsupported locally)\n" */
+/* 		       "l - set led values, needs 2 args\n" */
+/* 		       "v - read voltage\n" */
+/* 		       "i - read current\n" */
+/* 		       "r - get rail values\n" */
+/* 		       "s - set rail values - be REALLY CAREFULL! ( needs 2 args)\n" */
+/* 		       "d - get dip switch values\n" */
+/* 		       "u - check for usb cabble plugged in\n" */
+/* 		       "o - send char out ( needst args)\n"); */
+/* 		return -1; */
+/* 	} */
+
+	
+/* 	switch( *argv[1]){ */
+/* 	case 'w': */
+/* 		//printf("Read ID as %x\n", readbyte(fd, 0)); */
+/* 		printf("sorry you cant because it sucks\n"); */
+/* 		return -1; */
+/* 		break; */
+/* 	case 'l': */
+/* 		printf("Leds\n"); */
+/* 		if (argc!=3) */
+/* 		{ */
+/* 			printf("l takes another argument for the value\n"); */
+/* 			return -1; */
+/* 		} */
+/* 		printf("cmd:%d\n",LED); */
+/* 		printf("%d\n",setpins(fd,LED,atoi(argv[2]))); */
+/* 		return 0; */
+/* /\* 	case 'v': *\/ */
+/* /\* 		printf("%d\n", readword(fd, GETV)); *\/ */
+/* /\* 		return 0; *\/ */
+/* /\* 	case 'i': *\/ */
+/* /\* 		printf("%d\n", readword(fd, GETI)); *\/ */
+/* /\* 		return 0; *\/ */
+/* /\* 	case 's': *\/ */
+/* /\* 		if (argc!=3) *\/ */
+/* /\* 		{ *\/ */
+/* /\* 			printf("l takes another argument for the value\n"); *\/ */
+/* /\* 			return -1; *\/ */
+/* /\* 		} *\/ */
+/* /\* 		printf("%d\n", setpins(fd, SETRAILS,atoi(argv[2]))); *\/ */
+/* /\* 		return 0; *\/ */
+/* /\* 	case 'r': *\/ */
+/* /\* 		printf("%d\n", readbyte(fd, GETRAILS)); *\/ */
+/* /\* 		return 0; *\/ */
+
+/* /\* 	case 'x': *\/ */
+/* /\* 		printf("%d\n", readword(fd, GETRAILS)); *\/ */
+/* /\* 		return 0; *\/ */
+
+/* /\* 	case 'o': *\/ */
+/* /\* 		if (argc!=3) *\/ */
+/* /\* 		{ *\/ */
+/* /\* 			printf("l takes another argument for the value\n"); *\/ */
+/* /\* 			return -1; *\/ */
+/* /\* 		} *\/ */
+/* /\* 		printf("%d\n", setpins(fd,SENDSER,*argv[2])); *\/ */
+/* /\* 		return 0; *\/ */
+/* 	case 'd': */
+/* 		printf("%d\n", readbyte(fd, DIPSWITCH)); */
+/* 		return 0; */
+
+/* /\* 	case 'u': *\/ */
+/* /\* 		printf("%d\n", readbyte(fd, ISUSB)); *\/ */
+/* /\* 		return 0; *\/ */
+/* /\* 	case 'q': *\/ */
+/* /\* 		printf("break everything?\n"); *\/ */
+/* /\* 		printf("%d\n", readbyte(fd, 93)); *\/ */
+/* /\* 		break; *\/ */
+
+/* 	case 'e': */
+/* 		printf("Ah Ah Ah Ah, stayin' alive!\n"); */
+/* 		printf("%d\n",setpins(fd,BEEGEES,1)); */
+/* 		//sends alive packet! */
+/* 		break; */
+		
+/* 	default: */
+/* 		printf("not a recognised command go think again\n"); */
+/* 	} */
+
+/* 	return -1; */
+
+
+/* } */
+
+
 
 uint16_t readword(int fd, uint8_t cmd) {
 	signed int tmp;
@@ -49,125 +205,4 @@ char setpins( int fd, uint8_t command, uint8_t val )
 		return 1;
 	}
 	return 0;
-}
-
-
-int main( int argc, char** argv )
-{
-	int fd;			
-	printf("1\n");
-	fd = open( "/dev/i2c-1", O_RDWR );
-	printf("2\n");
-	if( fd == -1 )
-	{
-		fprintf( stderr, "Failed to open /dev/`i2c-1: %m\n" );
-		return 1;
-	}
-
-	printf("3\n");
-
-	if( ioctl( fd, I2C_SLAVE, ADDRESS ) < 0 )
-	{
-		fprintf( stderr, "Failed to set slave address: %m\n" );
-		return 2;
-	}
-
-	printf("4\n");
-
-	if( ioctl( fd, I2C_PEC, 1) < 0)
-	{
-		fprintf( stderr, "Failed to enable PEC\n");
-		return 3;
-	}
-
-
-	printf("5\n");
-//------------------------------
-	if (argc<2){ // check at least 1 arg
-		printf("incorrect args\n");
-		//printf("Usage: %s {w,l,v,i,r,s}\n");
-		printf("w - identify(currently unsupported locally)\n"
-		       "l - set led values, needs 2 args\n"
-		       "v - read voltage\n"
-		       "i - read current\n"
-		       "r - get rail values\n"
-		       "s - set rail values - be REALLY CAREFULL! ( needs 2 args)\n"
-		       "d - get dip switch values\n"
-		       "u - check for usb cabble plugged in\n"
-		       "o - send char out ( needst args)\n");
-		return -1;
-	}
-
-	
-	switch( *argv[1]){
-	case 'w':
-		//printf("Read ID as %x\n", readbyte(fd, 0));
-		printf("sorry you cant because it sucks\n");
-		return -1;
-		break;
-	case 'l':
-		printf("Leds\n");
-		if (argc!=3)
-		{
-			printf("l takes another argument for the value\n");
-			return -1;
-		}
-		printf("cmd:%d\n",LED);
-		printf("%d\n",setpins(fd,LED,atoi(argv[2])));
-		return 0;
-/* 	case 'v': */
-/* 		printf("%d\n", readword(fd, GETV)); */
-/* 		return 0; */
-/* 	case 'i': */
-/* 		printf("%d\n", readword(fd, GETI)); */
-/* 		return 0; */
-/* 	case 's': */
-/* 		if (argc!=3) */
-/* 		{ */
-/* 			printf("l takes another argument for the value\n"); */
-/* 			return -1; */
-/* 		} */
-/* 		printf("%d\n", setpins(fd, SETRAILS,atoi(argv[2]))); */
-/* 		return 0; */
-/* 	case 'r': */
-/* 		printf("%d\n", readbyte(fd, GETRAILS)); */
-/* 		return 0; */
-
-/* 	case 'x': */
-/* 		printf("%d\n", readword(fd, GETRAILS)); */
-/* 		return 0; */
-
-/* 	case 'o': */
-/* 		if (argc!=3) */
-/* 		{ */
-/* 			printf("l takes another argument for the value\n"); */
-/* 			return -1; */
-/* 		} */
-/* 		printf("%d\n", setpins(fd,SENDSER,*argv[2])); */
-/* 		return 0; */
-	case 'd':
-		printf("%d\n", readbyte(fd, DIPSWITCH));
-		return 0;
-
-/* 	case 'u': */
-/* 		printf("%d\n", readbyte(fd, ISUSB)); */
-/* 		return 0; */
-/* 	case 'q': */
-/* 		printf("break everything?\n"); */
-/* 		printf("%d\n", readbyte(fd, 93)); */
-/* 		break; */
-
-	case 'e':
-		printf("Ah Ah Ah Ah, stayin' alive!\n");
-		printf("%d\n",setpins(fd,BEEGEES,1));
-		//sends alive packet!
-		break;
-		
-	default:
-		printf("not a recognised command go think again\n");
-	}
-
-	return -1;
-
-
 }
