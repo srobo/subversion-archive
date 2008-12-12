5c8470544e67e7a88e705a399087d9f74fdcf500
rob <rob@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-12 01:47:48 +0000

    power-fw: Add comments to led.h describing its function.

--- boards/power/firmware/trunk/led.h
+++ boards/power/firmware/trunk/led.h
@@ -1,11 +1,20 @@
+/* Routines for controlling the LEDs */
 #ifndef __LED_H
 #define __LED_H
-
 #include <stdint.h>
-void led_init(void);
-void led_set(uint8_t val);
-uint8_t led_get(void);
 
+void led_init(void);
+
+/* Set the LEDs to the given value.
+ * Arguments:
+ *  - val: 4 least significant bits map to LEDs.
+ *         '0' for off.  '1' for on. */
+void led_set(uint8_t val);
+
+/* Get the current LED state.
+ * The 4 least significant bits of return value map to LEDs.
+ * '0' for off.  '1' for on. */
+uint8_t led_get(void);
 
 /* #define goff P5OUT |= 0x01 */
 /* #define gon P5OUT &= ~0x1 */
@@ -21,4 +30,5 @@ uint8_t led_get(void);
 /*          13       P1.1/TA0   I/O      LED1 */
 /*          14       P1.2/TA1   I/O      LED2 */
 /*          15       P1.3/TA2   I/O      LED3 */
-#endif
+
+#endif	/* __LED_H */
