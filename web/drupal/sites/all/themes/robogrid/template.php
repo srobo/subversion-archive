b0e3de01be9b14813125793c4fc8676ed59cd068
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-14 01:08:03 +0000

    drupal forum styling fixed - changed the function robogrid_preprocess_forum_topic_list() so that only the column headings that we want are output

--- web/drupal/sites/all/themes/robogrid/template.php
+++ web/drupal/sites/all/themes/robogrid/template.php
@@ -22,63 +22,23 @@ function robogrid_preprocess_forum_submitted(&$variables) {
  *
  * @see forum-topic-list.tpl.php
  * @see theme_forum_topic_list()
- *
+ */
 function robogrid_preprocess_forum_topic_list(&$variables) {
 	global $forum_topic_list_header;
 
 	// Create the tablesorting header.
 	$ts = tablesort_init($forum_topic_list_header);
-	$header = '';
+	$header = array();
+//	echo "forum_topic_list_header: ".print_r($forum_topic_list_header, true)."\nts: ".print_r($ts, true);
 	foreach ($forum_topic_list_header as $cell) {
-		$cell = tablesort_header($cell, $forum_topic_list_header, $ts);
+		$cell_html = tablesort_header($cell, $forum_topic_list_header, $ts);
 
-//		$header .= '<th>'.$cell['data'].'</th>';
-//		foreach($cell as $headings) {
-//			$header .= '<th>'.$headings['data'].'</th>';
-//		}
-//		print_r($forum_topic_list_header);
-//		print_r($ts);
-//		echo 'Cell: '.$cell.' header:'.$forum_topic_list_header.' ts: '.$ts;
-		$header .= _theme_table_cell($cell, TRUE);
+//		echo "\ncell: ".print_r($cell, true);
+		$header[$cell['data']]	= _theme_table_cell($cell_html, TRUE);
 	}
+//	echo "\nheader: ".print_r($header, true);
 
-	$variables['header'] = $header;
+	$variables['header'] = $header['&nbsp;'].$header['Topic'].$header['Last reply'].$header['Replies'];
 
-/*	if (!empty($variables['topics'])) {
-		$row = 0;
-		foreach ($variables['topics'] as $id => $topic) {
-			$variables['topics'][$id]->icon = theme('forum_icon', $topic->new, $topic->num_comments, $topic->comment_mode, $topic->sticky);
-			$variables['topics'][$id]->zebra = $row % 2 == 0 ? 'odd' : 'even';
-			$row++;
-
-			// We keep the actual tid in forum table, if it's different from the
-			// current tid then it means the topic appears in two forums, one of
-			// them is a shadow copy.
-			if ($topic->forum_tid != $variables['topic_id']) {
-				$variables['topics'][$id]->moved = TRUE;
-				$variables['topics'][$id]->title = check_plain($topic->title);
-				$variables['topics'][$id]->message = l(t('This topic has been moved'), "forum/$topic->forum_tid");
-			} else {
-				$variables['topics'][$id]->moved = FALSE;
-				$variables['topics'][$id]->title = l($topic->title, "node/$topic->nid");
-				$variables['topics'][$id]->message = '';
-			}
-			echo $variables['topics'][$id]->created;// = theme('forum_submitted_starter', $topic); // Added by probinson 20/11/08
-
-			$variables['topics'][$id]->last_reply = theme('forum_submitted', isset($topic->last_reply) ? $topic->last_reply : NULL);
-
-			$variables['topics'][$id]->new_text = '';
-			$variables['topics'][$id]->new_url = '';
-			if ($topic->new_replies) {
-				$variables['topics'][$id]->new_text = format_plural($topic->new_replies, '1 new', '@count new');
-				$variables['topics'][$id]->new_url = url("node/$topic->nid", array('query' => comment_new_page_count($topic->num_comments, $topic->new_replies, $topic), 'fragment' => 'new'));
-			}
-		}
-	} else {
-		// Make this safe for the template
-		$variables['topics'] = array();
-	}
-//*/
-//	$variables['pager'] = theme('pager', NULL, $variables['forum_per_page'], 0);
-//}
+}
 ?>
