790177b5bb613310826197e85458c9f4306613ab
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-14 01:48:18 +0000

    drupal forum comment.tpl.php minor tweaks to file whitespace

--- web/drupal/sites/all/themes/robogrid/comment.tpl.php
+++ web/drupal/sites/all/themes/robogrid/comment.tpl.php
@@ -1,13 +1,12 @@
 <?php
 // $Id: comment.tpl.php,v 1.7 2008/01/04 19:24:23 goba Exp $
 ?>
-  <div class="comment<?php print ' '. $status; ?>">
-    <?php if ($picture) {
-    print $picture;
-  } ?>
-    <div class="submitted"><?php print $submitted; ?></div>
-    <div class="content">
-     <?php print $content; ?>
-    </div>
-    <div class="reply-links"> <?php print $links; ?></div>
-  </div>
+<div class="comment<?php print ' '. $status; ?>">
+	<?php if ($picture)
+		print $picture; ?>
+	<div class="submitted"><?php print $submitted; ?></div>
+	<div class="content">
+	<?php print $content; ?>
+	</div>
+	<div class="reply-links"><?php print $links; ?></div>
+</div>
