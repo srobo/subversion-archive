989a9b1edf4276a0380d64dfea0107c5423ac322
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-14 13:49:40 +0000

    Drupal forum module - back to 6.6 now that the theming is done by theme files

--- web/drupal/modules/forum/forum-topic-list.tpl.php
+++ web/drupal/modules/forum/forum-topic-list.tpl.php
@@ -33,7 +33,7 @@
  * @see theme_forum_topic_list()
  */
 ?>
-<table id="forum-topic-<?php print $topic_id; ?>" class="forum-outer">
+<table id="forum-topic-<?php print $topic_id; ?>">
   <thead>
     <tr><?php print $header; ?></tr>
   </thead>
@@ -41,15 +41,19 @@
   <?php foreach ($topics as $topic): ?>
     <tr class="<?php print $topic->zebra;?>">
       <td class="icon"><?php print $topic->icon; ?></td>
-      <td class="title"><?php print $topic->title; if($topic->new_replies) print ''; ?><br /><?php print $topic->created; ?></td>
+      <td class="title"><?php print $topic->title; ?></td>
     <?php if ($topic->moved): ?>
       <td colspan="3"><?php print $topic->message; ?></td>
     <?php else: ?>
-
-      <td class="last-reply"><?php print $topic->last_reply; ?></td>
-	  <td class="replies" style="text-align:left;">
+      <td class="replies">
         <?php print $topic->num_comments; ?>
+        <?php if ($topic->new_replies): ?>
+          <br />
+          <a href="<?php print $topic->new_url; ?>"><?php print $topic->new_text; ?></a>
+        <?php endif; ?>
       </td>
+      <td class="created"><?php print $topic->created; ?></td>
+      <td class="last-reply"><?php print $topic->last_reply; ?></td>
     <?php endif; ?>
     </tr>
   <?php endforeach; ?>
