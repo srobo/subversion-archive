a237c68b892ae6f5b9567a375795dc4a4eadea25
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-22 02:49:42 +0000

    slug-br: Build scripts for flashb.

---
+++ slug/init-br/package/flashb/flashb.mk
@@ -0,0 +1,31 @@
+############################################################
+#
+# flashb
+#
+############################################################
+FLASHB_REVISION := 2196
+FLASHB_SVN := http://svn.srobo.org/slug/utils/flashb/trunk
+FLASHB_SOURCE := $(DL_DIR)/flashb-$(FLASHB_REVISION)
+FLASHB_DIR := $(BUILD_DIR)/flashb-$(FLASHB_REVISION)
+
+$(FLASHB_SOURCE)/.exported:
+	svn export -r $(FLASHB_REVISION) $(FLASHB_SVN) $(FLASHB_SOURCE)
+	touch $(FLASHB_SOURCE)/.exported
+
+$(FLASHB_DIR)/.source: $(FLASHB_SOURCE)/.exported
+	cp -r $(FLASHB_SOURCE) $(FLASHB_DIR)
+	touch $(FLASHB_DIR)/.source
+
+$(FLASHB_DIR)/flashb: $(FLASHB_DIR)/.source
+	( export PKG_CONFIG_LIBDIR=$(STAGING_DIR)/usr/local/lib/pkgconfig ; \
+	  export PKG_CONFIG_ARGS="--define-variable=prefix=$(STAGING_DIR)/usr/local" ; \
+	  LDFLAGS="-L $(STAGING_DIR)/usr/local/lib -L $(STAGING_DIR)/usr/lib" CFLAGS="-I $(STAGING_DIR)/usr/include" $(MAKE) CC=$(TARGET_CC) -C $(FLASHB_DIR) ; )
+
+$(TARGET_DIR)/usr/bin/flashb: $(FLASHB_DIR)/flashb
+	cp -a $(FLASHB_DIR)/flashb $(TARGET_DIR)/usr/bin/
+
+flashb: $(TARGET_DIR)/usr/bin/flashb
+
+ifeq ($(strip $(BR2_PACKAGE_FLASHB)),y)
+TARGETS+=flashb
+endif
