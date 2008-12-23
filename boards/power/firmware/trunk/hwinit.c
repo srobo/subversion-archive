fa5f4ba8132c2405fc00d00836ab4221aa56cf6f
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2008-12-23 14:58:39 +0000

    power firm slug rail not inited to input

--- boards/power/firmware/trunk/hwinit.c
+++ boards/power/firmware/trunk/hwinit.c
@@ -18,7 +18,8 @@ void init_cpu(void)
   BCSCTL1 = 0xC7;
   BCSCTL2 = 0;
 
-  P1DIR = P2DIR = P3DIR = P4DIR = P5DIR = P6DIR = 0;
+  P1DIR = P3DIR = P4DIR = P5DIR = P6DIR = 0;
+  P2DIR & 0x02;			/* try and keep slug alive */
 	P3SEL |= 0x0A;
   
 }
