77a0ae4432a57543c189a85f902ada24fd778bf1
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2009-01-05 01:13:31 +0000

    bom-scripts: Move the EAGLE parts list parsing into the parts_parser module.

--- boards/lib/bom-scripts/sr_lint.py
+++ boards/lib/bom-scripts/sr_lint.py
@@ -1,50 +1,31 @@
 #!/usr/bin/env python
-import sys, parts_db
+import os, sys, parts_db, parts_parser
 
-if(sys.argv.__len__() < 4):
-    print "Usage is:"
-    print sys.argv[0],"eagle-partlistin parts-db outfile"
+if len(sys.argv) < 3:
+    print "Usage:",os.path.basename(sys.argv[0]),"PARTSLIST SR_DB"
     print "Check all components in a parts list exist."
     sys.exit(1)
 
 inpath = sys.argv[1]
 srpath = sys.argv[2]
-outpath = sys.argv[3]
 
 lib = parts_db.Db(srpath)
-infile = open(inpath,"r") # eagle bom file
+parts = parts_parser.EagleParts(inpath)
 
 bom = {}
-fail = 0
 
-# skip eagle header
-for i in range(8):
-    l = infile.readline()
-    # line 5 starts with "EAGLE" in EAGLE parts lists.
-    if i == 4 and l[0:5] != "EAGLE":
-        print "Error: Parts list is not EAGLE parts list."
-        sys.exit(3)
-        
 error = 0
 found = 0
 
-for line in infile:
-    fields = line.split()
-    sr_id = fields[1].lower() # eg SRcr10k
-    board_id = fields[0].strip() # eg R1
-
-    if ( not (lib.has_key(sr_id))):
-        print "Error (%s): '%s' not in SR component database, please re-align your mind" % (board_id, sr_id)
-        fail = 1
-        error = error+1
+for id in parts.keys():
+    if not lib.has_key( parts[id] ):
+        print "Error (%s): '%s' not in SR component database, please re-align your mind" % (id, parts[id])
+        error = error + 1
     else:
-        #print sr_id ,"ok", line
-        bom.setdefault(sr_id,[])
-        bom[sr_id].append(board_id)
-        found = found +1
+        found = found + 1
 
-print found,"parts found in",srpath
-if (fail == 1):
+print found,"parts checked in",srpath
+if error > 0:
     print "Fail:", error, "parts not identified"
     sys.exit(2)
 
