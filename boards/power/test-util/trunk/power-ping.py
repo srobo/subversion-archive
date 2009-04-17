930ec864d85e336a856114d003a9180173950533
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2009-04-17 13:23:31 +0000

    power test - added a utility to send powerboard ping packets

---
+++ boards/power/test-util/trunk/power-ping.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import time
+import serial
+import sys
+
+
+port=sys.argv[1]#"/dev/ttyUSB0"
+tty = serial.Serial(port, baudrate=57600, rtscts=0, xonxoff=0, timeout=5)
+
+ping = 	\x7E\x00\x0D\x80\x00\x01\x02\x03\x04\x05\x06\x07\x00\xff\x01\x02\x00
+
+while 1:
+    time.sleep(0.5)
+    tty.write(ping)
+
+tty.close()
+
+
+
+# 	0x7E, //  framing byte
+# 	0, 0x0D, // length
+# 	0x80,             // API identifier for ive recived a packet
+# 	0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07, //# Source address -- this should be ignoored
+# 	0, //rssi -ignoored
+# 	0xff, // options -- ignoored
+# 	0x01,0x02, // the actuall data!!!
+# 	0x00};
