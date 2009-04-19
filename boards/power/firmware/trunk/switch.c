16469fb2468011d48e34bd58fcb5e74699126182
tbennellick <tbennellick@98f67ba8-b320-0410-bda0-b753e294843c>
2009-04-19 14:34:59 +0000

    power firmware - comp day, fix the 1sec bug

--- boards/power/firmware/trunk/switch.c
+++ boards/power/firmware/trunk/switch.c
@@ -27,9 +27,20 @@ interrupt (PORT2_VECTOR) port2_isr(void){
 	uint8_t p2ifg_l;
 	p2ifg_l = P2IFG; 	/* read only once just to be safe */
 	if (p2ifg_l &  0x08){ /* p2.3 interrupt hit  */
+
+		
+		TACTL &= ~TAIE;
+
+
+		TACCTL0 &= ~TAIE;
+		TACCTL1 &= ~CCIE;
+		TACCTL2 &= ~CCIE;
+		//TACTL TACCTL0 = TACCTL1 = TACCTL2 = TACCR1 = TACCR2 = TACTL =0;
+		
 		user_enable();
+		P1OUT |= 2;
 		pwr_set_motor(1);
-		button_pressed = 1;
+//		button_pressed = 1;
 		P2IFG &= ~0x08;	/* clear interrupt flag */
 	}else
 		P2IFG =0;	/* must have been another, that we dont care about */
