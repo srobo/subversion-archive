bb00ab3dce3af070b3e1eca3e747af30ee2c9d37
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2009-06-20 12:06:45 +0000

    motor-fw: #include from msp430-libc should be in '<>', not quotes

--- boards/motor/firmware/trunk/i2c.c
+++ boards/motor/firmware/trunk/i2c.c
@@ -14,7 +14,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 #include "common.h"
-#include "msp430/usci.h"
+#include <msp430/usci.h>
 #include <signal.h>
 #include <bool.h>
 #include "i2c.h"
