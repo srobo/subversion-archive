2c1d407c878464a42bca9290583e270cdc3cacd6
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2009-07-01 22:34:03 +0000

    flash430: Use io.h instead of device specific headers.

--- boards/lib-fw/flash430/trunk/dev.h
+++ boards/lib-fw/flash430/trunk/dev.h
@@ -1,33 +1,30 @@
 #ifndef __FLASH430_DEV_H
 #define __FLASH430_DEV_H
+#include <io.h>
 
 /* In the future, this may use the device peripheral constants, like
  * __MSP430_HAS_FLASH__.  But for now, we're doing it based on the
  * device */
 
 #ifdef __MSP430_2254__
-#include <msp430x22x4.h>
 #define FLASH_AREA_0 0xc000
 #define FLASH_AREA_1 0xe000
 #define FLASH_AREA_LEN 0x1e00
 #endif
 
 #ifdef __MSP430_2234__
-#include <msp430x22x4.h>
 #define FLASH_AREA_0 0xe000
 #define FLASH_AREA_1 0xf000
 #define FLASH_AREA_LEN 0xe00
 #endif
 
 #ifdef __MSP430_169__
-#include <msp430x16x.h>
 #define FLASH_AREA_0 0x1200
 #define FLASH_AREA_1 0x8900
 #define FLASH_AREA_LEN 0x7500
 #endif
 
 #ifdef __MSP430_1611__
-#include <msp430x16x.h>
 #define FLASH_AREA_0 0x4000
 #define FLASH_AREA_1 0xa000
 #define FLASH_AREA_LEN 0x5e00
