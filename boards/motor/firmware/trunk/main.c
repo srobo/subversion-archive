6f04a81939f2be99c3e73427e11a458560c58fcb
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2009-07-01 22:30:28 +0000

    motor-fw: Move ADC code into separate files.
    The ADC code wasn't written in the modular way that the rest of the
    code was written.  This patch moves it into a module of its own.

--- boards/motor/firmware/trunk/main.c
+++ boards/motor/firmware/trunk/main.c
@@ -22,13 +22,9 @@
 #include "flash430/i2c-flash.h"
 #include "leds.h"
 #include <signal.h>
-#include <msp430/adc10.h>
+#include "adc-10.h"
 
 static int i = 0;
-
-/* adc buffer, pointer */
-extern uint8_t adc_channel;
-extern uint16_t currents[2]; 
 
 /* Initialise the GPIO ports */
 void init_gpio( void );
@@ -48,37 +44,11 @@ int main( void )
 	motor_set( 0, 0, M_OFF );
 	motor_set( 1, 0, M_OFF );
 
-	/* start conversion */
-	adc_channel = 0;	
-	ADC10CTL0 |= ENC 
-			| ADC10SC;	/* Turn on ADC & enable conversion, start */
-
-	
 	while(1) {
-		if( i2c_flash_received ) {
-				flash_switchover();
-		}
-		if(ADC10BUSY)
-		{
-			currents[adc_channel] = ADC10MEM;
-			if(adc_channel)
-			{
-				adc_channel = 0x00;	//change the channel
-				ADC10CTL1 &= 0xF000;
-				ADC10CTL1 |= INCH_12;
-				ADC10CTL0 |= ENC 
-					| ADC10SC;	/* Turn on ADC & enable conversion, start */
-			}
-			else
-			{
-				adc_channel = 0x01;	//change the channel
-				ADC10CTL1 &= 0xF000;
-				ADC10CTL1 |= INCH_13;
-				ADC10CTL0 |= ENC 
-					| ADC10SC;	/* Turn on ADC & enable conversion, start */
-			}
-				
-		}
+		if( i2c_flash_received )
+			flash_switchover();
+
+		adc10_proc();
 	}
 }
 
@@ -92,6 +62,7 @@ void init( void )
 	flash_init();
 	i2c_flash_init();
 	i2c_init();
+	adc10_init();
 
 	eint();
 }
@@ -115,22 +86,6 @@ void init_gpio( void )
 	P4DIR |= 0x60;
 	P4OUT &= ~0x60;
 
-	/* Enable A12 and A13 as analogue inputs */
-	ADC10CTL0 |= 	SREF_0
-			| ADC10SHT_DIV64
-			| ADC10ON
-			| REF2_5V;
-
-	ADC10CTL1 |=  INCH_12
-			| ADC10DIV_0
-			| ADC10SSEL_0
-			| CONSEQ_0;
-
-	ADC10AE1 |= 0x30;
-
-	ADC10DTC1 = 0x00;
-
-	/* End Current Sense Initialisation */
 	/* Debug light off */
 	FLAG_OFF();
 }
