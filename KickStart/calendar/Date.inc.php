80762214dde3c8a17fab62d6c0db77d56a06fbc5
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2009-08-23 16:09:53 +0000

    KickStart calendar: update the date and time for SR2010

--- KickStart/calendar/Date.inc.php
+++ KickStart/calendar/Date.inc.php
@@ -1,5 +1,5 @@
 <?
-$Kick_DATE	= "2008-9-28 12:15";
-$Comp_DATE	= "2009-4-18";
-$Easter_DATE	= "2009-4-12";
+$Kick_DATE	= "2008-9-19 14:0";
+$Comp_DATE	= "2010-4-18";
+$Easter_DATE	= "2010-4-4";
 ?>
