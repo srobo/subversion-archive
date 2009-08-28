93c9e4ba675f12cbcd2252feb22a804a14048a50
jmorse <jmorse@98f67ba8-b320-0410-bda0-b753e294843c>
2009-08-28 14:00:33 +0000

    Implement sobel edge detection - not perfect right now, edges with a large difference in value can overflow the byte result.

--- boards/slug/vision/robovis/visfunc.cpp
+++ boards/slug/vision/robovis/visfunc.cpp
@@ -14,23 +14,50 @@
 
 #include "visfunc.h"
 
-/* Ensure that "MAX" is what we expect it to be */
+/* Ensure that "MAX" and "MIN" are what we expect them to be */
 #if defined(MAX)
 #undef MAX
 #endif
+#if defined(MIN)
+#undef MIN
+#endif
+#define MIN(a,b)        (((a)<(b))?(a):(b))
 #define        MAX(a,b)        (((a)>(b))?(a):(b))
 
 #define template_size 5
+#define sobel_size 5
 #define image_depth 8
 
 unsigned char bias[template_size][template_size];
+int sobel_x[sobel_size][sobel_size];
+int sobel_y[sobel_size][sobel_size];
 
+int
+fact(int num)
+{
+	int i, accumulator;
+
+	accumulator = 1;
+	for (i = 1; i <= num; i++)
+		accumulator *= i;
+
+	return accumulator;
+}
+
+int
+pascal(int width, int pos)
+{
+
+	return (fact(width)) / (fact(width - pos) * fact(pos));
+}
+	
 void
 vis_start()
 {
-	static int started = 0;
-	int x, y, i, j;
 	double calc;
+	static int started = 0;
+	int x, y, i, j, max;
+	float calcf;
 
 	if (started)
 		return;
@@ -49,6 +76,40 @@ vis_start()
 		y =  -((template_size-1)/2);
 	}
 
+	max = 0;
+	for (i = 0; i < sobel_size; i++) {
+		for (j = 0; j < sobel_size; j++) {
+			sobel_y[i][j] = pascal(sobel_size-1, j) * 
+			(pascal(sobel_size-2, i) - pascal(sobel_size-2, i-1));
+			max = MAX(max, sobel_y[i][j]);
+		}
+	}
+
+	for (i = 0; i < sobel_size; i++) {
+		for (j = 0; j < sobel_size; j++) {
+			sobel_x[i][j] = pascal(sobel_size-1, i) * 
+			(pascal(sobel_size-2, j) - pascal(sobel_size-2, j-1));
+		}
+	}
+
+	/* We have the maximum integer number used in the template, now scale
+	 * all values to be in the range 0-255. Negative parts will scale to
+	 * 0-(-255). */
+
+	calcf = 255 / max;
+	for (i = 0; i < sobel_size; i++) {
+		for (j = 0; j < sobel_size; j++) {
+			sobel_y[i][j] = (int) (sobel_y[i][j] * calcf);
+		}
+	}
+
+	for (i = 0; i < sobel_size; i++) {
+		for (j = 0; j < sobel_size; j++) {
+			sobel_x[i][j] = (int) (sobel_x[i][j] * calcf);
+		}
+	}
+	
+
 	return;
 }
 
@@ -61,7 +122,7 @@ vis_do_smooth(IplImage *src)
 	IplImage *dst;
 	unsigned char *in;
 	unsigned char *out;
-	int i, j, k, l, x, y, in_stride, out_stride;
+	int i, j, k, l, in_stride, out_stride;
 	int template_border;
 	int accumul;
 
@@ -95,12 +156,10 @@ vis_do_smooth(IplImage *src)
 			 * surrounding area. Efficiency might be reached by
 			 * caching previous values... but then bias dies */
 
-			x = i - template_border;
-			y = j - template_border;
 			accumul = 0;
 			for (k = 0; k < template_size; k++) {
 				for (l = 0; l < template_size; l++) {
-					accumul += get(x+k,y+l) * bias[k][l];
+					accumul += get(i+k,j+l) * bias[k][l];
 				}
 			}
 
@@ -139,7 +198,7 @@ vis_do_roberts_edge_detection(IplImage *src)
 	IplImage *dst;                  
 	unsigned char *in;
 	unsigned char *out;
-	int i, j, x, y, in_stride, out_stride;
+	int i, j, in_stride, out_stride;
 	int diff1, diff2;
 
 	vis_start();
@@ -167,16 +226,77 @@ vis_do_roberts_edge_detection(IplImage *src)
 
 	for (j = 0; j < dst->height; j++) {
 		for (i = 0; i < dst->width; i++) {
-			x = i;
-			y = j;
 
-			diff1 = get(x, y) - get(x+1, y+1);
-			diff2 = get(x+1, y) - get(x, y+1);
+			diff1 = get(i, j) - get(i+1, j+1);
+			diff2 = get(i+1, j) - get(i, j+1);
 
 			diff1 = abs(diff1);
 			diff2 = abs(diff2);
 
-			put(x,y) = MAX(diff1, diff2);
+			put(i,j) = MAX(diff1, diff2);
+		}
+	}
+
+	return dst;
+#undef get
+#undef put
+}
+
+IplImage *
+vis_do_sobel_edge_detection(IplImage *src)
+{
+#define get(x, y) *(in + ((y) * in_stride) + (x))
+#define put(x, y) *(out + ((y) * out_stride) + (x))
+	CvSize sz;
+	IplImage *dst;  
+	unsigned char *in;
+	unsigned char *out;
+	int i, j, k, l, in_stride, out_stride, border_size;
+	int accuml_x, accuml_y;
+ 
+	vis_start(); 
+
+	if (src->nChannels != 1) {
+		fprintf(stderr, "vis_do_sobel_edge_detection: bad chans %d\n",
+							src->nChannels);
+		exit(1);
+	}
+
+	border_size = (sobel_size-1)/2;
+	sz.width = src->width - (sobel_size-1);
+	sz.height = src->height - (sobel_size-1);
+
+	dst = cvCreateImage(sz, image_depth, 1);
+	if (!dst) {     
+		fprintf(stderr, "vis_do_sobel_edge_detection: "
+				"can't create image\n");
+		exit(1);                
+	}
+
+	in = (unsigned char*) src->imageData;
+	out = (unsigned char*) dst->imageData;
+	in_stride = src->widthStep;
+	out_stride = dst->widthStep;
+
+	for (j = 0; j < dst->height; j++) {
+		for (i = 0; i < dst->width; i++) {
+			accuml_x = accuml_y = 0;
+
+			for (k = 0; k < template_size; k++) {
+				for (l = 0; l < template_size; l++) {
+					accuml_x += get(i+k,j+l)*sobel_x[k][l];
+					accuml_y += get(i+k,j+l)*sobel_y[k][l];
+				}
+			}
+
+			accuml_x >>= 8;
+			accuml_y >>= 8;
+//			accuml_x /= (sobel_size*sobel_size);
+//			accuml_y /= (sobel_size*sobel_size);
+			accuml_x = abs(accuml_x);
+			accuml_y = abs(accuml_y);
+
+			put(i, j) = MAX(accuml_x, accuml_y);
 		}
 	}
 
--- boards/slug/vision/robovis/visfunc.h
+++ boards/slug/vision/robovis/visfunc.h
@@ -5,6 +5,8 @@
 
 IplImage *vis_do_smooth(IplImage *src);
 IplImage *vis_do_roberts_edge_detection(IplImage *src);
+IplImage *vis_do_sobel_edge_detection(IplImage *src);
+
 
 
 #endif /* _BOARDS_SLUG_VISION_ROBOVIS_VISFUNC_H_ */
