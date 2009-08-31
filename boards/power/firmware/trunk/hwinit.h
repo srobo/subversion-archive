08e640f9baa0508ffdaaae6a2a3e322a02b2eed3
rspanton <rspanton@98f67ba8-b320-0410-bda0-b753e294843c>
2009-08-31 21:27:20 +0000

    power-fw: Move main initialisation code into main.c

--- boards/power/firmware/trunk/hwinit.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HWINIT_H
-#define __HWINIT_H
-#include"device.h"
-#include <io.h>
-
-void init_cpu(void);
-
-#endif
--- boards/power/firmware/trunk/main.c
+++ boards/power/firmware/trunk/main.c
@@ -1,6 +1,5 @@
 #include"device.h"
 #include<io.h>
-#include"hwinit.h"
 #include<signal.h>
 #include"led.h"
 #include <stdint.h>
@@ -18,12 +17,26 @@
 
 int dummy =0; 			/* dummy variable to fix gdb bug */
 
+void init(void);
+void init_cpu(void);
+
 int main(void)
 {
 	/* Make sure that the slug rail is always on */
 	P2DIR |= 1;
 	P2OUT |= 1;
 
+	init();
+
+	while(1)
+	{
+		if( i2c_flash_received )
+			flash_switchover();
+ 	}
+}
+
+void init(void)
+{
 	init_cpu();
 	led_init();
 	switch_init();
@@ -38,11 +51,32 @@ int main(void)
 	flash_init();
 	i2c_flash_init();
 	eint();			/* enable global interrupts */
-
-	while(1)
-	{
-		if( i2c_flash_received )
-			flash_switchover();
- 	}
 }
 
+void init_cpu(void)
+{
+	/* No watchdog reset */
+	WDTCTL = WDTPW + WDTHOLD;
+
+	/* oscillator */
+	/* setup dco */
+	DCOCTL = 0xe0;// max freq | max mod
+	BCSCTL1 = XT2OFF | XTS | 0xC7 | RSEL2 | RSEL1 | RSEL0;
+	BCSCTL2 = 0;
+
+#define XTAL
+#ifdef XTAL
+	/*  */
+	BCSCTL1 =  RSEL2 | RSEL1 | RSEL0; /* enable xt2 */
+	BCSCTL2 = SELS;		    /* smclk sourced from xt2 */
+#endif
+
+	P1DIR = P3DIR = P4DIR = P5DIR = P6DIR = 0;
+	P1OUT = P3OUT =  P4OUT =  P5OUT =  P6OUT = 0x00;
+
+	/* Don't touch bit 0 of P2OUT */
+	P2DIR &= 1;
+	P2OUT &= 1;
+
+	P3SEL |= 0x0A;
+}
