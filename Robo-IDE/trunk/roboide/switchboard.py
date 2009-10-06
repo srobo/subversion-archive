cac415b11fd495656da6e90b5155ae027feb9215
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2009-10-06 01:46:51 +0000

    RoboIDE: switchboard: remove extra whitespace, don't instanciate the message feed twice onto the same variable

--- Robo-IDE/trunk/roboide/switchboard.py
+++ Robo-IDE/trunk/roboide/switchboard.py
@@ -4,7 +4,7 @@ import model
 import user as srusers
 
 sr_message_feed = "http://sr2010messages.blogspot.com/feeds/posts/default?alt=rss"
-sr_timeline_events = [{"date":"September 19, 2009", "title":"Kickstart", "desc":"Kickstart: Competition launch"}, 
+sr_timeline_events = [{"date":"September 19, 2009", "title":"Kickstart", "desc":"Kickstart: Competition launch"},
 			{"date":"November 11, 2009", "title":"Build Chasis", "desc":"You should have the chasis of the robot built"},
 			{"date":"November 20, 2009", "title":"Build Ramp", "desc":"You should have built a practice ramp by now"},
 			{"date":"February 20, 2010", "title":"Tech Day", "desc":"A chance to get help with the programming/electronics"},
@@ -15,23 +15,20 @@ sr_timeline_end = "April 20, 2010"
 #We want to sanitize <div>s from feeds
 feedparser._HTMLSanitizer.acceptable_elements.remove("a")
 
-# The maximum number of messages to display 
+# The maximum number of messages to display
 MAX_MESSAGES = 5
 # The maixum length of a message to display (in chars)
 MAX_MESSAGE_LENGTH = 100
 
 class SRMessageFeed():
 	def __init__(self):
-		self.fd = feedparser.parse(sr_message_feed)	
+		self.fd = feedparser.parse(sr_message_feed)
 
 	def GetMessages(self):
 		msglist = [{"title":e.title, "link":e.link, "author":e.author_detail.name,
 					"date":e.date, "body":e.description[0:MAX_MESSAGE_LENGTH].strip()}
 				for e in self.fd.entries]
 		return dict(msgs=msglist[0:MAX_MESSAGES])
-
-# Single instance of the message feed shared by all users
-sfd =  SRMessageFeed()	
 
 class StudentBlogPosts():
 	def __init__(self):
@@ -62,12 +59,12 @@ class StudentBlogPosts():
 		print self.msgs
 		return dict(msgs=self.msgs)
 
-# Single instance of the message feed shared by all users
-sfd =  SRMessageFeed()	
+# Single instance of the message feeds shared by all users
+sfd = SRMessageFeed()
 sbp = StudentBlogPosts()
 
 class Switchboard(object):
-	
+
 	user = srusers.User()
 	def __init__(self):
 		self.feedurl = "default.rss"
@@ -80,7 +77,7 @@ class Switchboard(object):
 	@expose("json")
 	@srusers.require(srusers.in_team())
 	def milestones(self):
-		#TOOD: get this dynamically, perhaps from an xml file? 
+		#TOOD: get this dynamically, perhaps from an xml file?
 		return dict(start=sr_timeline_start, end=sr_timeline_end, events=sr_timeline_events)
 
 	@expose("json")
@@ -88,12 +85,12 @@ class Switchboard(object):
 	def setblogfeed(self, feedurl):
 		"""Returns the new rss feed url associated with a user. If
 		the user has not previously registered a url, it will assign a
-		new row in the table, else it will update their existing entry.	
-		"""	
+		new row in the table, else it will update their existing entry.
+		"""
 		#get the current user
 		cur_user = srusers.get_curuser()
 
-		# grab the sql record, edit the url, commit it 
+		# grab the sql record, edit the url, commit it
 		try:
 			r = model.UserBlogFeeds.selectBy(user=cur_user)
 			try:
@@ -113,15 +110,15 @@ class Switchboard(object):
 		except:
 			return dict(feedurl="", valid=0, error=1)
 		else:
-			return dict(feedurl=row.url, valid=int(row.valid), error=0)	
+			return dict(feedurl=row.url, valid=int(row.valid), error=0)
 
 	@expose("json")
 	@srusers.require(srusers.in_team())
 	def getblogfeed(self):
-		"""Returns the current rss feed url associated with a user. If 
+		"""Returns the current rss feed url associated with a user. If
 		the user has not previously registered a url, it will return a
 		blank string.
-		"""	
+		"""
 		#get the current user
 		cur_user = srusers.get_curuser()
 
@@ -134,11 +131,11 @@ class Switchboard(object):
 				#did not get a single result, error!
 				return dict(feedurl="", valid=0, error=1)
 		except:
-			# the record doesn't exist, return blank 
+			# the record doesn't exist, return blank
 			return dict(feedurl="", valid=0, error=1)
 
 		return dict(feedurl=row.url, valid=int(row.valid), error=0)
-	
+
 	@expose("json")
 	@srusers.require(srusers.in_team())
 	def getblogposts(self):
