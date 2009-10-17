ce19ffa3cd29efe9dd884e171e72f82d779e9fe2
plaw <plaw@98f67ba8-b320-0410-bda0-b753e294843c>
2009-10-17 15:24:56 +0000

    RoboIDE: projpage: Ensure some more variables that shuold be local are

--- Robo-IDE/trunk/roboide/static/javascript/projpage.js
+++ Robo-IDE/trunk/roboide/static/javascript/projpage.js
@@ -467,11 +467,11 @@ ProjFileList.prototype._onclick = function(ev) {
 	ev.preventDefault();
 	ev.stopPropagation();
 
-	mods = ev.modifier();
+	var mods = ev.modifier();
 
-	src = ev.src();
-	kind = getNodeAttribute( src, "ide_kind" );
-	path = getNodeAttribute( src, "ide_path" );
+	var src = ev.src();
+	var kind = getNodeAttribute( src, "ide_kind" );
+	var path = getNodeAttribute( src, "ide_path" );
 
 	if( mods["ctrl"] || mods["meta"] ) {	//meta is the mac command button, which they use like Win/Linux uses ctrl
 		if( !this._is_file_selected(path) )
